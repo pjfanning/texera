9e7515fa10bfcedb1619eacfc264bab636d67ff0
Shengquan Ni <121989255@qq.com>
2021-09-16 16:18:20 -0700

    Update workflow-websocket.service.spec.ts

--- core/new-gui/src/app/workspace/service/workflow-websocket/workflow-websocket.service.spec.ts
+++ core/new-gui/src/app/workspace/service/workflow-websocket/workflow-websocket.service.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from "@angular/core/testing";
-
+import { HttpClient } from "@angular/common/http";
+import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
 import { WorkflowWebsocketService } from "./workflow-websocket.service";
 import { WorkflowActionService } from "../workflow-graph/model/workflow-action.service";
 import { WorkflowUtilService } from "../workflow-graph/util/workflow-util.service";
@@ -10,21 +11,26 @@ import { StubOperatorMetadataService } from "../operator-metadata/stub-operator-
 
 describe("WorkflowWebsocketService", () => {
   let service: WorkflowWebsocketService;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [
+        {
+          provide: OperatorMetadataService,
+          useClass: StubOperatorMetadataService,
+        },
         WorkflowActionService,
         WorkflowUtilService,
         UndoRedoService,
         JointUIService,
-        WorkflowWebsocketService,
-        {
-          provide: OperatorMetadataService,
-          useClass: StubOperatorMetadataService,
-        }
+        WorkflowWebsocketService
       ],
     });
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
     service = TestBed.inject(WorkflowWebsocketService);
   });
 
