09de4e1340fbb09ff4f61a331db2a810d4f3da44
Shengquan Ni <121989255@qq.com>
2021-09-16 16:57:40 -0700

    fix other tests

--- core/new-gui/src/app/workspace/service/workflow-console/workflow-console.service.spec.ts
+++ core/new-gui/src/app/workspace/service/workflow-console/workflow-console.service.spec.ts
@@ -1,12 +1,31 @@
 import { TestBed } from "@angular/core/testing";
 
 import { WorkflowConsoleService } from "./workflow-console.service";
+import { WorkflowWebsocketService } from "../workflow-websocket/workflow-websocket.service";
+import { WorkflowActionService } from "../workflow-graph/model/workflow-action.service";
+import { WorkflowUtilService } from "../workflow-graph/util/workflow-util.service";
+import { UndoRedoService } from "../undo-redo/undo-redo.service";
+import { JointUIService } from "../joint-ui/joint-ui.service";
+import { OperatorMetadataService } from "../operator-metadata/operator-metadata.service";
+import { StubOperatorMetadataService } from "../operator-metadata/stub-operator-metadata.service";
 
 describe("WorkflowConsoleService", () => {
   let service: WorkflowConsoleService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        WorkflowActionService,
+        WorkflowUtilService,
+        UndoRedoService,
+        JointUIService,
+        WorkflowWebsocketService,
+        {
+          provide: OperatorMetadataService,
+          useClass: StubOperatorMetadataService,
+        },
+      ],
+    });
     service = TestBed.inject(WorkflowConsoleService);
   });
 
