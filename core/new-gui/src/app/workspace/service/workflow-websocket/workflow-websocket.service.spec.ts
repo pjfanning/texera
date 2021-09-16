69634414d100829427a623eb3cb41692643d7820
Shengquan Ni <121989255@qq.com>
2021-09-16 16:31:56 -0700

    Update workflow-websocket.service.spec.ts

--- core/new-gui/src/app/workspace/service/workflow-websocket/workflow-websocket.service.spec.ts
+++ core/new-gui/src/app/workspace/service/workflow-websocket/workflow-websocket.service.spec.ts
@@ -1,36 +1,32 @@
 import { TestBed } from "@angular/core/testing";
-import { HttpClient } from "@angular/common/http";
-import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
 import { WorkflowWebsocketService } from "./workflow-websocket.service";
 import { WorkflowActionService } from "../workflow-graph/model/workflow-action.service";
-import { WorkflowUtilService } from "../workflow-graph/util/workflow-util.service";
-import { UndoRedoService } from "../undo-redo/undo-redo.service";
-import { JointUIService } from "../joint-ui/joint-ui.service";
-import { OperatorMetadataService } from "../operator-metadata/operator-metadata.service";
-import { StubOperatorMetadataService } from "../operator-metadata/stub-operator-metadata.service";
+import { Observable, of } from "rxjs";
+import { WorkflowMetadata } from "src/app/dashboard/type/workflow-metadata.interface";
+
+class StubWorkflowActionService {
+  constructor() {}
+  public getWorkflowMetadata(): WorkflowMetadata {
+    return { name: "", wid: 1, creationTime: undefined, lastModifiedTime: undefined };
+  }
+  public workflowMetaDataChanged(): Observable<void> {
+    return of();
+  }
+}
 
 describe("WorkflowWebsocketService", () => {
   let service: WorkflowWebsocketService;
-  let httpClient: HttpClient;
-  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
       providers: [
         {
-          provide: OperatorMetadataService,
-          useClass: StubOperatorMetadataService,
+          provide: WorkflowActionService,
+          useClass: StubWorkflowActionService,
         },
-        WorkflowActionService,
-        WorkflowUtilService,
-        UndoRedoService,
-        JointUIService,
-        WorkflowWebsocketService
+        WorkflowWebsocketService,
       ],
     });
-    httpClient = TestBed.inject(HttpClient);
-    httpTestingController = TestBed.inject(HttpTestingController);
     service = TestBed.inject(WorkflowWebsocketService);
   });
 
