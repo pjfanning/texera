e185af9583b1b32d9f868391685bf80ffc1960ba
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2021-10-29 01:02:30 -0700

    fix format

--- core/amber/src/main/scala/edu/uci/ics/texera/web/service/WorkflowJobService.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/web/service/WorkflowJobService.scala
@@ -6,14 +6,28 @@ import edu.uci.ics.amber.engine.architecture.controller.{ControllerConfig, Workf
 import edu.uci.ics.amber.engine.common.client.AmberClient
 import edu.uci.ics.amber.engine.common.virtualidentity.WorkflowIdentity
 import edu.uci.ics.texera.web.{SnapshotMulticast, TexeraWebApplication}
-import edu.uci.ics.texera.web.model.websocket.event.{ExecutionStatusEnum, TexeraWebSocketEvent, Uninitialized, WorkflowStateEvent}
-import edu.uci.ics.texera.web.model.websocket.request.{CacheStatusUpdateRequest, ModifyLogicRequest, ResultExportRequest, WorkflowExecuteRequest}
+import edu.uci.ics.texera.web.model.websocket.event.{
+  ExecutionStatusEnum,
+  TexeraWebSocketEvent,
+  Uninitialized,
+  WorkflowStateEvent
+}
+import edu.uci.ics.texera.web.model.websocket.request.{
+  CacheStatusUpdateRequest,
+  ModifyLogicRequest,
+  ResultExportRequest,
+  WorkflowExecuteRequest
+}
 import edu.uci.ics.texera.web.model.websocket.response.ResultExportResponse
 import edu.uci.ics.texera.web.resource.WorkflowWebsocketResource
 import edu.uci.ics.texera.workflow.common.WorkflowContext
 import edu.uci.ics.texera.workflow.common.workflow.WorkflowCompiler.ConstraintViolationException
 import edu.uci.ics.texera.workflow.common.workflow.WorkflowInfo.toJgraphtDAG
-import edu.uci.ics.texera.workflow.common.workflow.{WorkflowCompiler, WorkflowInfo, WorkflowRewriter}
+import edu.uci.ics.texera.workflow.common.workflow.{
+  WorkflowCompiler,
+  WorkflowInfo,
+  WorkflowRewriter
+}
 import org.jooq.types.UInteger
 import rx.lang.scala.subjects.BehaviorSubject
 import rx.lang.scala.subscriptions.CompositeSubscription
