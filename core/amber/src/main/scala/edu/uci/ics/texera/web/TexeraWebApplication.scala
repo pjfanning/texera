70ece9d5354f6ba4863f215ae280ad7dfbb16f5f
sadeemsaleh <sadeem.alsudais@gmail.com>
2022-07-16 17:45:15 -0700

    added mongodb cleanup logic

--- core/amber/src/main/scala/edu/uci/ics/texera/web/TexeraWebApplication.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/web/TexeraWebApplication.scala
@@ -4,9 +4,11 @@ import akka.actor.{ActorSystem, Cancellable}
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import com.github.dirkraft.dropwizard.fileassets.FileAssetsBundle
 import com.github.toastshaman.dropwizard.auth.jwt.JwtAuthFilter
+import com.mongodb.BasicDBObject
+import com.mongodb.client.model.Indexes
+import com.mongodb.client.{MongoClient, MongoClients, MongoCollection, MongoCursor, MongoDatabase}
 import edu.uci.ics.amber.engine.architecture.controller.{ControllerConfig, Workflow}
 import edu.uci.ics.amber.engine.common.AmberUtils
-import edu.uci.ics.amber.engine.common.client.AmberClient
 import edu.uci.ics.texera.Utils
 import edu.uci.ics.texera.web.auth.JwtAuth.jwtConsumer
 import edu.uci.ics.texera.web.auth.{
@@ -34,14 +36,15 @@ import org.eclipse.jetty.websocket.server.WebSocketUpgradeFilter
 import org.glassfish.jersey.media.multipart.MultiPartFeature
 import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature
 
-import java.time.Duration
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration.FiniteDuration
+import scala.concurrent.duration.{DurationInt, FiniteDuration}
 import java.time.Duration
-
 import edu.uci.ics.amber.engine.common.client.AmberClient
+import edu.uci.ics.texera.web.TexeraWebApplication.scheduleRecurringCallThroughActorSystem
 import org.apache.commons.jcs3.access.exception.InvalidArgumentException
+import org.bson.Document
 
+import java.util.concurrent.TimeUnit
 import scala.annotation.tailrec
 
 object TexeraWebApplication {
@@ -180,6 +183,54 @@ class TexeraWebApplication extends io.dropwizard.Application[TexeraWebConfigurat
     environment.jersey.register(classOf[WorkflowVersionResource])
     environment.jersey.register(classOf[ProjectResource])
     environment.jersey.register(classOf[WorkflowExecutionsResource])
+
+    // create a mongo collection that handles cleanup of other stored collections only when mode is mongodb
+    if (AmberUtils.amberConfig.getString("storage.mode").equalsIgnoreCase("mongodb")) {
+      // create the new catalog collection
+      val url: String = AmberUtils.amberConfig.getString("storage.mongodb.url")
+      val databaseName: String = AmberUtils.amberConfig.getString("storage.mongodb.database")
+      val client: MongoClient = MongoClients.create(url)
+      val database: MongoDatabase = client.getDatabase(databaseName)
+      val catalogCollectionName: String =
+        AmberUtils.amberConfig.getString("storage.mongodb.cleanup.catalog-collection-name")
+      val catalogCollection: MongoCollection[Document] =
+        database.getCollection(catalogCollectionName)
+      val timeToLive: Int = AmberUtils.amberConfig.getInt("storage.mongodb.cleanup.ttl-in-seconds")
+      // if the collection doesn't exist, then create it and add an index on the timestamp column
+      if (catalogCollection.countDocuments() == 0) {
+        catalogCollection.createIndex(Indexes.ascending("createdAt"))
+      }
+
+      scheduleRecurringCallThroughActorSystem(
+        2.seconds,
+        AmberUtils.amberConfig
+          .getInt("storage.mongodb.cleanup.collection-check-interval-in-seconds")
+          .seconds
+      ) {
+        cleanOldCollections(database, catalogCollection, timeToLive)
+      }
+
+    }
   }
 
+  /**
+    * This function is called periodically and checks all expired collections and deletes them
+    * MongoDB doesn't have an API of drop collection where collection name in (from a subquery), so the implementation is to retrieve
+    * the entire list of those documents that have expired, then loop the list to drop them one by one
+    */
+  def cleanOldCollections(
+      database: MongoDatabase,
+      catalogCollection: MongoCollection[Document],
+      timeToLive: Int
+  ): Unit = {
+    val condition: BasicDBObject = new BasicDBObject
+    condition.put(
+      "createdAt",
+      new BasicDBObject("$lt", System.currentTimeMillis() - TimeUnit.SECONDS.toMillis(timeToLive))
+    )
+    val expiredDocuments: MongoCursor[Document] = catalogCollection.find(condition).cursor()
+    while (expiredDocuments.hasNext) {
+      database.getCollection(expiredDocuments.next().getString("collectionName")).drop()
+    }
+  }
 }
