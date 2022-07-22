bb6aa6e191186202f440810f72ff107843b7d639
Liqin Ye <liqiny@uci.edu>
2022-07-21 17:36:26 -0700

    add view icon

--- core/new-gui/src/app/dashboard/component/feature-container/saved-workflow-section/ngbd-modal-workflow-executions/ngbd-modal-workflow-executions.component.ts
+++ core/new-gui/src/app/dashboard/component/feature-container/saved-workflow-section/ngbd-modal-workflow-executions/ngbd-modal-workflow-executions.component.ts
@@ -23,6 +23,7 @@ export class NgbdModalWorkflowExecutionsComponent implements OnInit {
   public executionsTableHeaders: string[] = [
     "",
     "",
+    "",
     "Execution#",
     "Username",
     "Name",
