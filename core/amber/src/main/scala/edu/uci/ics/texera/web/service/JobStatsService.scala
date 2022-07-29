465e26ca01991d0543fbbd8872cb6ed844534b7c
Ethan Deng <ethand.2091@gmail.com>
2022-07-29 10:31:36 -0700

    Revert "built operator json nodes and prints"
    This reverts commit 38b6b3846162a06a3543a0327a24200df731631e.

--- core/amber/src/main/scala/edu/uci/ics/texera/web/service/JobStatsService.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/web/service/JobStatsService.scala
@@ -1,13 +1,18 @@
 package edu.uci.ics.texera.web.service
 
-import com.fasterxml.jackson.databind.node.ObjectNode
-import com.fasterxml.jackson.databind.ObjectMapper
-import edu.uci.ics.amber.engine.architecture.controller.ControllerEvent.{WorkflowCompleted, WorkflowStatusUpdate}
+import edu.uci.ics.amber.engine.architecture.controller.ControllerEvent.{
+  WorkflowCompleted,
+  WorkflowStatusUpdate
+}
 import edu.uci.ics.amber.engine.architecture.controller.promisehandlers.FatalErrorHandler.FatalError
 import edu.uci.ics.amber.engine.common.client.AmberClient
 import edu.uci.ics.texera.Utils
 import edu.uci.ics.texera.web.SubscriptionManager
-import edu.uci.ics.texera.web.model.websocket.event.{OperatorStatistics, OperatorStatisticsUpdateEvent, TexeraWebSocketEvent}
+import edu.uci.ics.texera.web.model.websocket.event.{
+  OperatorStatistics,
+  OperatorStatisticsUpdateEvent,
+  TexeraWebSocketEvent
+}
 import edu.uci.ics.texera.web.storage.{JobStateStore, WorkflowStateStore}
 import edu.uci.ics.texera.web.workflowruntimestate.WorkflowAggregatedState.{ABORTED, COMPLETED}
 
@@ -31,28 +36,6 @@ class JobStatsService(
                 stats.inputCount,
                 stats.outputCount
               )
-
-              //only proceed if stats.state is COMPLETED/ABORTED?
-              var mapper: ObjectMapper = new ObjectMapper()
-              //TODO: these two belong somewhere else so a new one doesn't get created every time, specific to an execution
-              var execution: ObjectNode = mapper.createObjectNode() //TODO: obtain the eId here? or let mongo manager obtain it?
-              var operator: ObjectNode = mapper.createObjectNode()
-
-              var opStats:ObjectNode  = mapper.createObjectNode() //mongo manager will need to check if operator ID already exists in the array
-
-
-              opStats.put("state", Utils.aggregatedStateToString(stats.state))
-              opStats.put("inputCount", stats.inputCount)
-              opStats.put("outputCount", stats.outputCount)
-
-              operator.set(x._1, opStats)
-
-//              execution.set("operators", ) //need to build an ArrayNode to store operators
-//              execution.put("expiredAfterSeconds", 172800)
-
-              println("------------------")
-              println(operator.toString)
-              println("------------------")
               (x._1, res)
           })
         )
