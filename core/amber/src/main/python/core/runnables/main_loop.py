79d33f5d2806694c60182af099bea4031b40e689
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2022-11-29 22:58:39 -0800

    refactor python print handling logic, detach from loguru

--- core/amber/src/main/python/core/runnables/main_loop.py
+++ core/amber/src/main/python/core/runnables/main_loop.py
@@ -1,3 +1,4 @@
+import datetime
 import threading
 import traceback
 import typing
@@ -26,6 +27,7 @@ from core.runnables.data_processor import DataProcessor
 from core.util import StoppableQueueBlockingRunnable, get_one_of, set_one_of
 from core.util.customized_queue.queue_base import QueueElement
 from core.util.print_writer.print_log_handler import PrintLogHandler
+from core.util.print_writer.replace_print import replace_print
 from proto.edu.uci.ics.amber.engine.architecture.worker import (
     ControlCommandV2,
     LocalOperatorExceptionV2,
@@ -63,6 +65,7 @@ class MainLoop(StoppableQueueBlockingRunnable):
                 ),
             )
         )
+
         self.data_processor = DataProcessor(self.context)
         threading.Thread(target=self.data_processor.run, daemon=True).start()
 
@@ -94,8 +97,8 @@ class MainLoop(StoppableQueueBlockingRunnable):
         processing a DataElement.
         """
         while (
-            not self._input_queue.is_control_empty()
-            or self.context.pause_manager.is_paused()
+                not self._input_queue.is_control_empty()
+                or self.context.pause_manager.is_paused()
         ):
             next_entry = self.interruptible_get()
             self._process_control_element(next_entry)
@@ -124,7 +127,7 @@ class MainLoop(StoppableQueueBlockingRunnable):
         )
 
     def process_control_payload(
-        self, tag: ActorVirtualIdentity, payload: ControlPayloadV2
+            self, tag: ActorVirtualIdentity, payload: ControlPayloadV2
     ) -> None:
         """
         Process the given ControlPayload with the tag.
@@ -158,8 +161,8 @@ class MainLoop(StoppableQueueBlockingRunnable):
                 self.cast_tuple_to_match_schema(output_tuple, schema)
                 self.context.statistics_manager.increase_output_tuple_count()
                 for (
-                    to,
-                    batch,
+                        to,
+                        batch,
                 ) in self.context.tuple_to_batch_converter.tuple_to_batch(output_tuple):
                     batch.schema = self.context.operator_manager.operator.output_schema
                     self._output_queue.put(DataElement(tag=to, payload=batch))
@@ -222,7 +225,7 @@ class MainLoop(StoppableQueueBlockingRunnable):
             )
 
     def _process_sender_change_marker(
-        self, sender_change_marker: SenderChangeMarker
+            self, sender_change_marker: SenderChangeMarker
     ) -> None:
         """
         Upon receipt of a SenderChangeMarker, change the current input link to the
@@ -321,7 +324,7 @@ class MainLoop(StoppableQueueBlockingRunnable):
         """
         self._print_log_handler.flush()
         if self.context.state_manager.confirm_state(
-            WorkerState.RUNNING, WorkerState.READY
+                WorkerState.RUNNING, WorkerState.READY
         ):
             self.context.pause_manager.record_request(PauseType.USER_PAUSE, True)
             self.context.state_manager.transit_to(WorkerState.PAUSED)
@@ -354,12 +357,27 @@ class MainLoop(StoppableQueueBlockingRunnable):
             if field_type == pyarrow.binary():
                 output_tuple[field_name] = b"pickle    " + pickle.dumps(field_value)
 
+    def _send_console_message(self, msg: str, level="PRINT"):
+        self._async_rpc_client.send(
+            ActorVirtualIdentity(name="CONTROLLER"),
+            set_one_of(
+                ControlCommandV2,
+                PythonConsoleMessageV2(
+                    timestamp=datetime.datetime.now(), level=level,
+                    message=msg
+                ),
+            )
+        )
+
     def _switch_context(self):
         with self.context.tuple_processing_manager.context_switch_condition:
-            self.context.tuple_processing_manager.context_switch_condition.notify()
-            self.context.tuple_processing_manager.context_switch_condition.wait()
+            with replace_print(self._send_console_message, "PRINT"):
+                self.context.tuple_processing_manager.context_switch_condition.notify()
+                self.context.tuple_processing_manager.context_switch_condition.wait()
 
     def _check_and_report_exception(self):
         if self.context.exception_manager.has_exception():
             self.report_exception(self.context.exception_manager.get_exc_info())
             self._pause()
+
+
--- core/amber/src/main/python/texera_run_python_worker.py
+++ core/amber/src/main/python/texera_run_python_worker.py
@@ -1,10 +1,8 @@
-import contextlib
 import sys
 
 from loguru import logger
 
 from core.python_worker import PythonWorker
-from core.util.print_writer.stream_to_logger_adaptor import StreamToLoggerAdaptor
 
 
 def init_loguru_logger(stream_log_level) -> None:
@@ -29,7 +27,6 @@ def init_loguru_logger(stream_log_level) -> None:
 if __name__ == "__main__":
     init_loguru_logger(sys.argv[3])
     # redirect user's print into logger
-    with contextlib.redirect_stdout(StreamToLoggerAdaptor()):
-        PythonWorker(
-            host="localhost", input_port=int(sys.argv[1]), output_port=int(sys.argv[2])
-        ).run()
+    PythonWorker(
+        host="localhost", input_port=int(sys.argv[1]), output_port=int(sys.argv[2])
+    ).run()
