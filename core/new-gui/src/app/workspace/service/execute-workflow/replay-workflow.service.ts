56462504969a6ef930cd1491401016cf8be7a176
Zuozhi Wang <zuozhiw@gmail.com>
2023-01-27 14:42:20 -0800

    more UI changes

--- core/new-gui/src/app/workspace/service/execute-workflow/replay-workflow.service.ts
+++ core/new-gui/src/app/workspace/service/execute-workflow/replay-workflow.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { Observable, Subject } from "rxjs";
 import { NotificationService } from "src/app/common/service/notification/notification.service";
 import { WorkflowWebsocketService } from "../workflow-websocket/workflow-websocket.service";
+import { ExecutionState } from "../../types/execute-workflow.interface";
 
 export const DISPLAY_WORKFLOW_EXECUTION_REPLAY = "display_workflow_execution_replay";
 
@@ -12,6 +13,10 @@ export class ReplayWorkflowService {
   public history: readonly number[] = [];
   private displayWorkflowReplay = new Subject<string>();
   public operatorInfo: string = "";
+  public selectedIndex = -1;
+  
+  public replayStarted = false;
+  public replayEnded = false;
 
   constructor(private workflowWebsocketService: WorkflowWebsocketService, private notification: NotificationService) {
     workflowWebsocketService.subscribeToEvent("WorkflowInteractionHistoryEvent").subscribe(e => {
@@ -21,6 +26,16 @@ export class ReplayWorkflowService {
     workflowWebsocketService.subscribeToEvent("WorkflowAdditionalOperatorInfoEvent").subscribe(e =>{
       this.operatorInfo = e.data;
     });
+    this.history = [1,5, 20, 30, 40]
+
+    workflowWebsocketService.subscribeToEvent("WorkflowStateEvent").subscribe(e => {
+      if (e.state === ExecutionState.Paused || e.state === ExecutionState.Completed) {
+        if (this.replayStarted) {
+          this.replayStarted = false;
+          this.replayEnded = true;
+        }
+      }
+    })
   }
 
   public displayWorkflowReplayPanel(): void {
@@ -32,6 +47,9 @@ export class ReplayWorkflowService {
   }
 
   public selectReplayPoint(index: number): void {
+    this.selectedIndex = index;
+    this.replayStarted = true;
+    this.replayEnded = false;
     this.workflowWebsocketService.send("WorkflowReplayRequest", { replayPos: index });
     this.notification.info("replaying time point " + this.history[index] + "s");
   }
