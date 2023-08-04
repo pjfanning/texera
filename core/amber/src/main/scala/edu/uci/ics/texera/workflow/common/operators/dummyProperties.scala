77dce1cd5529f407615fad1e8be0c76661895723
vthai321 <70456187+vthai321@users.noreply.github.com>
2023-08-03 18:25:37 -0700

    Add files via upload
    Added the dummyProperties file, which contains a trait for a dummy property that the OperatorDescriptor class uses. This dummy property is a non-required text field that allows a user to write down extra properties that are present in an external operator (Knime, Rapidminer) that aren't present in Texera.

---
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/common/operators/dummyProperties.scala
@@ -0,0 +1,13 @@
+package edu.uci.ics.texera.workflow.common.operators
+
+import com.fasterxml.jackson.annotation.JsonPropertyDescription
+import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaTitle
+import edu.uci.ics.texera.workflow.common.metadata.annotations.AutofillAttributeName
+import com.fasterxml.jackson.annotation.JsonProperty
+trait dummyProperties {
+  @JsonProperty(value = "Extra Functionalities", required = false)
+  @JsonSchemaTitle("Extra Functionalities")
+  @JsonPropertyDescription("Write values of extra functionalities here.")
+  @AutofillAttributeName var valuesOfFunctionalities: String = ""
+}
+
