d61a038c028997218a332b1b432a12b76cc7449b
vthai321 <70456187+vthai321@users.noreply.github.com>
2023-08-03 22:59:37 -0700

    Update and rename dummyProperties.scala to DummyProperties.scala

--- core/amber/src/main/scala/edu/uci/ics/texera/workflow/common/operators/DummyProperties.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/common/operators/DummyProperties.scala
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonPropertyDescription
 import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaTitle
 import edu.uci.ics.texera.workflow.common.metadata.annotations.AutofillAttributeName
 import com.fasterxml.jackson.annotation.JsonProperty
-trait dummyProperties {
+trait DummyProperties {
   @JsonProperty(value = "Extra Functionalities", required = false)
   @JsonSchemaTitle("Extra Functionalities")
   @JsonPropertyDescription("Write values of extra functionalities here.")
