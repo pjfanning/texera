089b684b7bbffaaf281afc4e229b3f9255f303b1
Bob <bobbaicloudwithpants@gmail.com>
2023-09-25 13:40:08 -0700

    fix conflict

--- core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/visualization/dumbbellPlot/DumbbellPlotOpDesc.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/visualization/dumbbellPlot/DumbbellPlotOpDesc.scala
@@ -44,15 +44,15 @@ class DumbbellPlotOpDesc extends VisualizationOperator with PythonOperatorDescri
   @AutofillAttributeName
   var categoryColumnName: String = ""
 
-  @JsonProperty(value = "firstCategory", required = true)
-  @JsonSchemaTitle("1st Category Value")
-  @JsonPropertyDescription("the value of the first category")
-  var firstCategory: String = ""
+  @JsonProperty(value = "startValue", required = true)
+  @JsonSchemaTitle("Start Value")
+  @JsonPropertyDescription("the start point value of each dumbbell")
+  var startValue: String = ""
 
-  @JsonProperty(value = "secondCategory", required = true)
-  @JsonSchemaTitle("2nd Category Value")
-  @JsonPropertyDescription("the value of the second category")
-  var secondCategory: String = ""
+  @JsonProperty(value = "endValue", required = true)
+  @JsonSchemaTitle("End Value")
+  @JsonPropertyDescription("the end value of each dumbbell")
+  var endValue: String = ""
 
   @JsonProperty(value = "measurementColumnName", required = true)
   @JsonSchemaTitle("Measurement Column Name")
@@ -60,11 +60,11 @@ class DumbbellPlotOpDesc extends VisualizationOperator with PythonOperatorDescri
   @AutofillAttributeName
   var measurementColumnName: String = ""
 
-  @JsonProperty(value = "entityColumnName", required = true)
-  @JsonSchemaTitle("Entity Column Name")
-  @JsonPropertyDescription("the column name of the entity being compared")
+  @JsonProperty(value = "comparedColumnName", required = true)
+  @JsonSchemaTitle("Compared Column Name")
+  @JsonPropertyDescription("the column name that is being compared")
   @AutofillAttributeName
-  var entityColumnName: String = ""
+  var comparedColumnName: String = ""
 
   override def getOutputSchema(schemas: Array[Schema]): Schema = {
     Schema.newBuilder.add(new Attribute("html-content", AttributeType.STRING)).build
@@ -82,61 +82,61 @@ class DumbbellPlotOpDesc extends VisualizationOperator with PythonOperatorDescri
   override def numWorkers() = 1
 
   def createPlotlyFigure(): String = {
-    val categoryValues = firstCategory + ", " + secondCategory
+    val dumbbellValues = startValue + ", " + endValue
 
     s"""
-     |
-     |        entityNames = list(table['${entityColumnName}'].unique())
-     |        categoryValues = [${categoryValues}]
-     |        filtered_table = table[(table['${entityColumnName}'].isin(entityNames)) &
-     |                    (table['${categoryColumnName}'].isin(categoryValues))]
-     |
-     |        # Create the dumbbell plot using Plotly
-     |        fig = go.Figure()
-     |
-     |        for entity in entityNames:
-     |          entity_data = filtered_table[filtered_table['${entityColumnName}'] == entity]
-     |          fig.add_trace(go.Scatter(x=entity_data['${measurementColumnName}'],
-     |                             y=[entity]*2,
-     |                             mode='lines+markers+text',
-     |                             name=entity,
-     |                             text=entity_data['${categoryColumnName}'],
-     |                             textposition='top center',
-     |                             marker=dict(size=10)))
-     |
-     |          fig.update_layout(title="${title}",
-     |                  xaxis_title="${measurementColumnName}",
-     |                  yaxis_title="${entityColumnName}",
-     |                  yaxis=dict(categoryorder='array', categoryarray=entityNames))
-     |""".stripMargin
+       |
+       |        entityNames = list(table['${comparedColumnName}'].unique())
+       |        categoryValues = [${dumbbellValues}]
+       |        filtered_table = table[(table['${comparedColumnName}'].isin(entityNames)) &
+       |                    (table['${categoryColumnName}'].isin(categoryValues))]
+       |
+       |        # Create the dumbbell plot using Plotly
+       |        fig = go.Figure()
+       |
+       |        for entity in entityNames:
+       |          entity_data = filtered_table[filtered_table['${comparedColumnName}'] == entity]
+       |          fig.add_trace(go.Scatter(x=entity_data['${measurementColumnName}'],
+       |                             y=[entity]*2,
+       |                             mode='lines+markers+text',
+       |                             name=entity,
+       |                             text=entity_data['${categoryColumnName}'],
+       |                             textposition='top center',
+       |                             marker=dict(size=10)))
+       |
+       |          fig.update_layout(title="${title}",
+       |                  xaxis_title="${measurementColumnName}",
+       |                  yaxis_title="${comparedColumnName}",
+       |                  yaxis=dict(categoryorder='array', categoryarray=entityNames))
+       |""".stripMargin
   }
 
   override def generatePythonCode(operatorSchemaInfo: OperatorSchemaInfo): String = {
     s"""
-     |from pytexera import *
-     |
-     |import plotly.express as px
-     |import plotly.graph_objects as go
-     |import plotly.io
-     |import numpy as np
-     |
-     |class ProcessTableOperator(UDFTableOperator):
-     |    def render_error(self, error_msg):
-     |        return '''<h1>DumbbellPlot is not available.</h1>
-     |                  <p>Reason is: {} </p>
-     |               '''.format(error_msg)
-     |
-     |    @overrides
-     |    def process_table(self, table: Table, port: int) -> Iterator[Optional[TableLike]]:
-     |        if table.empty:
-     |           yield {'html-content': self.render_error("input table is empty.")}
-     |           return
-     |        ${createPlotlyFigure()}
-     |        # convert fig to html content
-     |        html = plotly.io.to_html(fig, include_plotlyjs='cdn', auto_play=False)
-     |        yield {'html-content': html}
-     |
-     |""".stripMargin
+       |from pytexera import *
+       |
+       |import plotly.express as px
+       |import plotly.graph_objects as go
+       |import plotly.io
+       |import numpy as np
+       |
+       |class ProcessTableOperator(UDFTableOperator):
+       |    def render_error(self, error_msg):
+       |        return '''<h1>DumbbellPlot is not available.</h1>
+       |                  <p>Reason is: {} </p>
+       |               '''.format(error_msg)
+       |
+       |    @overrides
+       |    def process_table(self, table: Table, port: int) -> Iterator[Optional[TableLike]]:
+       |        if table.empty:
+       |           yield {'html-content': self.render_error("input table is empty.")}
+       |           return
+       |        ${createPlotlyFigure()}
+       |        # convert fig to html content
+       |        html = plotly.io.to_html(fig, include_plotlyjs='cdn', auto_play=False)
+       |        yield {'html-content': html}
+       |
+       |""".stripMargin
   }
 
   override def chartType(): String = VisualizationConstants.HTML_VIZ
