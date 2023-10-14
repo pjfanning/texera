c55a6939ac4e7b6a8a23251e5e83b833a70717a7
Shengquan Ni <shengqun@uci.edu>
2023-10-14 16:53:09 -0700

    Update WorkflowReplayManager.scala

--- core/amber/src/main/scala/edu/uci/ics/texera/web/service/WorkflowReplayManager.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/web/service/WorkflowReplayManager.scala
@@ -60,7 +60,7 @@ class WorkflowReplayManager(client:AmberClient, stateStore: JobStateStore, perio
     currentGapIdx = 0
     TexeraWebApplication.scheduleCallThroughActorSystem(1.millis){
       while(stateStore.jobMetadataStore.getState.state != COMPLETED){
-        Thread.sleep(Random.nextInt(4000) + 1000)
+        Thread.sleep(30000)
         client.executeAsync(actor => {
         val time = System.currentTimeMillis() - startTime
         val id = generateCheckpointId
