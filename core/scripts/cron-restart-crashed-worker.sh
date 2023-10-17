7bf19b12306880c3f643ec3a77e22be39718938e
Shengquan Ni <13672781+shengquan-ni@users.noreply.github.com>
2023-10-17 12:13:35 -0700

    Update cron-restart-crashed-worker.sh

--- core/scripts/cron-restart-crashed-worker.sh
+++ core/scripts/cron-restart-crashed-worker.sh
@@ -5,14 +5,17 @@ if jps -m | grep -q "TexeraWebApplication"; then
 
   # Check if TexeraRunWorker is missing
   if ! jps -m | grep -q "TexeraRunWorker"; then
-    echo "TexeraRunWorker is missing. Restarting..."
-
-    # Restart TexeraRunWorker
-    cd "$(dirname "$0")"
-    cd ../
-    ./scripts/worker.sh >/dev/null
-
-    echo "TexeraRunWorker restarted."
+    echo "TexeraRunWorker is missing. Will check again after 30 seconds."
+    sleep 30
+    if ! jps -m | grep -q "TexeraRunWorker"; then
+        echo "TexeraRunWorker is still missing. Restarting..."
+        # Restart TexeraRunWorker
+        cd "$(dirname "$0")"
+        cd ../
+        ./scripts/worker.sh >/dev/null
+        echo "TexeraRunWorker restarted."
+    else
+      echo "TexeraRunWorker is running after the first check."
   else
     echo "TexeraRunWorker is already running."
   fi
