dbf7df30ddde033848f1725f72232f2bee0732f7
mengw15 <simonmengwang123@gmail.com>
2023-11-06 20:59:33 -0800

    add upload file for the add new version button

--- core/new-gui/src/app/dashboard/user/component/user-dataset/user-dataset-view/ngbd-model-dataset-file-add/ngbd-model-dataset-file-add.component.ts
+++ core/new-gui/src/app/dashboard/user/component/user-dataset/user-dataset-view/ngbd-model-dataset-file-add/ngbd-model-dataset-file-add.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { FileUploader } from "ng2-file-upload";
+import { FileUploadItem } from "../../../../type/dashboard-file.interface";
+import { UserFileUploadService } from "../../../../service/user-file/user-file-upload.service";
 import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
 import { DatasetService } from "../../../../service/user-dataset/dataset.service";
 import { NgbActiveModal, NgbModal } from "@ng-bootstrap/ng-bootstrap";
+
+
 
 @UntilDestroy()
 @Component({
@@ -11,12 +16,62 @@ import { NgbActiveModal, NgbModal } from "@ng-bootstrap/ng-bootstrap";
 })
 export class NgbdModelDatasetFileAddComponent implements OnInit {
   versionNotes: string = '';
+  public haveDropZoneOver: boolean = false;
+
+  public uploader: FileUploader = new FileUploader({ url: "" });
 
   constructor(
-    public activeModal: NgbActiveModal
+    public activeModal: NgbActiveModal, private userFileUploadService: UserFileUploadService
   ) {}
 
-  ngOnInit(): void {
-      
+  ngOnInit(): void {}
+
+  // will be replaced by new service function
+  
+  public getFileArray(): ReadonlyArray<Readonly<FileUploadItem>> {
+    return this.userFileUploadService.getFilesToBeUploaded();
   }
+
+  public getFileArrayLength(): number {
+    return this.userFileUploadService.getFilesToBeUploaded().length;
+  }
+
+  public deleteFile(fileUploadItem: FileUploadItem): void {
+    this.userFileUploadService.removeFileFromUploadArray(fileUploadItem);
+  }
+
+  public uploadAllFiles(): void {
+    this.userFileUploadService.uploadAllFiles();
+  }
+
+  public isUploadAllButtonDisabled(): boolean {
+    return this.userFileUploadService.getFilesToBeUploaded().every(fileUploadItem => fileUploadItem.isUploadingFlag);
+  }
+
+  public haveFileOver(fileOverEvent: boolean): void {
+    this.haveDropZoneOver = fileOverEvent;
+  }
+
+  public getFileDropped(fileDropEvent: File[]): void {
+    for (let i = 0; i < fileDropEvent.length; i++) {
+      const file: File | null = fileDropEvent[i];
+      if (file !== null) {
+        this.userFileUploadService.addFileToUploadArray(file);
+      }
+    }
+
+    this.uploader.clearQueue();
+  }
+
+  public handleClickUploadFile(clickUploadEvent: Event): void {
+    const fileList: FileList | null = (clickUploadEvent as any).target.files;
+    if (fileList === null) {
+      throw new Error("browser upload does not work as intended");
+    }
+
+    for (let i = 0; i < fileList.length; i++) {
+      this.userFileUploadService.addFileToUploadArray(fileList[i]);
+    }
+  }
+
 }
