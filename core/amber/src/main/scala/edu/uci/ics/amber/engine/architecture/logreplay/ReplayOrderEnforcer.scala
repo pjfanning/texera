add25a715ec88a0ad0ee5e5c796da62bdfc13b42
Shengquan Ni <13672781+shengquan-ni@users.noreply.github.com>
2023-12-04 22:09:56 -0800

    Update ReplayOrderEnforcer.scala

--- core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/logreplay/ReplayOrderEnforcer.scala
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/logreplay/ReplayOrderEnforcer.scala
@@ -1,6 +1,5 @@
 package edu.uci.ics.amber.engine.architecture.logreplay
 
-import edu.uci.ics.amber.engine.architecture.common.ProcessingStepCursor.INIT_STEP
 import edu.uci.ics.amber.engine.common.ambermessage.ChannelID
 
 import scala.collection.mutable
@@ -12,39 +11,38 @@ class ReplayOrderEnforcer(
     replayTo: Long,
     var onComplete: () => Unit
 ) extends OrderEnforcer {
-  private var currentStep: Long = INIT_STEP - 1
   private var currentChannelID: ChannelID = _
   var isCompleted: Boolean = startStep > replayTo
 
   // restore replay progress by dropping some of the entries
-  while (channelStepOrder.nonEmpty && currentStep < startStep) {
+  while (channelStepOrder.nonEmpty && channelStepOrder.head.steps <= startStep) {
     forwardNext()
   }
 
   private def forwardNext(): Unit = {
     if (channelStepOrder.nonEmpty) {
       val nextStep = channelStepOrder.dequeue()
-      currentStep = nextStep.steps
       currentChannelID = nextStep.channelID
     }
   }
 
   def canProceed(channelID: ChannelID): Boolean = {
     val step = logManager.getStep
-    while (step > currentStep) {
+    // release the next log record if the step matches
+    if (channelStepOrder.nonEmpty && channelStepOrder.head.steps == step) {
       forwardNext()
     }
-    if (currentChannelID != channelID) {
-      false
-    } else {
-      if (step == this.replayTo || channelStepOrder.isEmpty) {
-        isCompleted = true
-        if (onComplete != null) {
-          onComplete()
-          onComplete = null
-        }
+    // two cases to terminate replay:
+    // 1. no next log record with step > current step, which means further processing is not logged.
+    // 2. current step == replayTo, no need to continue.
+    if (channelStepOrder.isEmpty || step == replayTo) {
+      isCompleted = true
+      if (onComplete != null) {
+        onComplete()
+        onComplete = null // make sure the onComplete is called only once.
       }
-      true
     }
+    // only proceed if the current channel ID matches the channel ID of the log record
+    currentChannelID == channelID
   }
 }
