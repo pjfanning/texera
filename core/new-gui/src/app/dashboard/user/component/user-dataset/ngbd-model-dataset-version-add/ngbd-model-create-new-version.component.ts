498182083b79277cfe197f6d0dc3ee845363b965
Jiadong Bai <bobbaicloudwithpants@gmail.com>
2023-12-27 11:25:53 -0800

    cleanup css

--- core/new-gui/src/app/dashboard/user/component/user-dataset/ngbd-model-dataset-version-add/ngbd-model-create-new-version.component.ts
+++ core/new-gui/src/app/dashboard/user/component/user-dataset/ngbd-model-dataset-version-add/ngbd-model-create-new-version.component.ts
@@ -44,63 +44,16 @@ export class NgbdModelCreateNewVersion implements OnInit {
         }
     }
 
-    // will be replaced by new service function
-
-    public getFileArray(): FileUploadItem[] {
-        return this.filesToBeUploaded;
-    }
-
-    public getFileArrayLength(): number {
-        return this.filesToBeUploaded.length;
-    }
-
-    public deleteNewFile(removedFile: FileUploadItem): void {
-        this.filesToBeUploaded = this.filesToBeUploaded.filter(file => file !== removedFile);
-    }
-
-    public deleteOldFile(removedFile: DatasetVersionHierarchyNode): void {
-      if (this.baseVersion) {
-        this.baseVersion.versionHierarchyRoot = this.baseVersion.versionHierarchyRoot
-            ? this.baseVersion.versionHierarchyRoot.filter(file => file !== removedFile) : undefined;
-        if (removedFile.dir) {
-          this.removedOldFiles.push(removedFile.dir + "/" + removedFile.name);
-        } else {
-          this.removedOldFiles.push(removedFile.name)
-        }
-      }
+    onFilesChanged(files: FileUploadItem[]) {
+        // Handle the changed files
+        this.filesToBeUploaded = files
     }
 
     public isCreateButtonDisabled(): boolean {
         return this.filesToBeUploaded.every(fileUploadItem => fileUploadItem.isUploadingFlag);
     }
 
-    public haveFileOver(fileOverEvent: boolean): void {
-        this.haveDropZoneOver = fileOverEvent;
-    }
-
-    public getFileDropped(fileDropEvent: File[]): void {
-        for (let i = 0; i < fileDropEvent.length; i++) {
-            const file: File | null = fileDropEvent[i];
-            if (file !== null) {
-                this.filesToBeUploaded.push(UserFileUploadService.createFileUploadItem(file));
-            }
-        }
-
-        this.uploader.clearQueue();
-    }
-
-    public handleClickUploadFile(clickUploadEvent: Event): void {
-        const fileList: FileList | null = (clickUploadEvent as any).target.files;
-        if (fileList === null) {
-            throw new Error("browser upload does not work as intended");
-        }
-
-        for (let i = 0; i < fileList.length; i++) {
-            this.filesToBeUploaded.push(UserFileUploadService.createFileUploadItem(fileList[i]));
-        }
-    }
-
-    public createVersion(): void {
+    public onClickCreateVersion(): void {
       if (this.did) {
         let files = new Array(this.filesToBeUploaded.length);
 
