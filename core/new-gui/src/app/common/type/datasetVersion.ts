5d2e6288aa711dbae24274368a02b0d8dfc77fc4
Jiadong Bai <bobbaicloudwithpants@gmail.com>
2024-01-03 15:10:57 -0800

    fix line

--- core/new-gui/src/app/common/type/datasetVersion.ts
+++ core/new-gui/src/app/common/type/datasetVersion.ts
@@ -16,7 +16,6 @@ export interface DatasetVersionFileTreeNode {
   type: 'file' | 'directory';
   children?: DatasetVersionFileTreeNode[]; // Only populated if 'type' is 'directory'
   parentDir: string;
-  displayableName: string;
 }
 export function parseFileTreeToNodes(hierarchy: DatasetVersionFileTree, parentDir: string = "/"): DatasetVersionFileTreeNode[] {
   const isDirectory = (node: DatasetVersionFileTree | string): node is DatasetVersionFileTree => {
@@ -31,14 +30,12 @@ export function parseFileTreeToNodes(hierarchy: DatasetVersionFileTree, parentDi
         type: "directory",
         children: Object.keys(node).map(key => parseHierarchyToNode(node[key], key, dir)),
         parentDir: currentDir,
-        displayableName: convertFileNameToDisplayableName(nodeName)
       };
     } else {
       return {
         name: nodeName,
         type: "file",
         parentDir: currentDir,
-        displayableName: convertFileNameToDisplayableName(nodeName)
       };
     }
   };
@@ -48,30 +45,6 @@ export function parseFileTreeToNodes(hierarchy: DatasetVersionFileTree, parentDi
   }
 
   return Object.keys(hierarchy).map(key => parseHierarchyToNode(hierarchy[key], key, parentDir));
-}
-
-
-function convertFileNameToDisplayableName(name: string): string {
-  const nameLimit = 10;
-  // Check if the name is already 10 characters or less
-  if (name.length <= nameLimit) {
-    return name;
-  }
-
-  // Split the name into base and extension
-  const lastDotIndex = name.lastIndexOf('.');
-  const base = name.substring(0, lastDotIndex);
-  const extension = lastDotIndex !== -1 ? name.substring(lastDotIndex) : '';
-
-  // Shorten the base part if necessary
-  if (base.length > nameLimit) {
-    // If there is an extension, leave space for it and '...'
-    const shortenedBase = extension ? base.substring(0, nameLimit-3) : base.substring(0, nameLimit);
-    return `${shortenedBase}...${extension}`;
-  } else {
-    // If the base is within limits, but the whole name isn't
-    return `${base.substring(0, nameLimit-3)}...${extension}`;
-  }
 }
 
 
