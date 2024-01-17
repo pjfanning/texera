e94664aad8cbe519a843eb22a5d9a52c184a51bf
Shengquan Ni <13672781+shengquan-ni@users.noreply.github.com>
2024-01-17 11:42:54 -0800

    Update TakeGlobalCheckpointHandler.scala

--- core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/controller/promisehandlers/TakeGlobalCheckpointHandler.scala
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/controller/promisehandlers/TakeGlobalCheckpointHandler.scala
@@ -4,19 +4,10 @@ import com.twitter.util.{Future, Promise}
 import edu.uci.ics.amber.engine.architecture.controller.ControllerAsyncRPCHandlerInitializer
 import edu.uci.ics.amber.engine.architecture.controller.promisehandlers.EpochMarkerHandler.PropagateChannelMarker
 import edu.uci.ics.amber.engine.architecture.controller.promisehandlers.TakeGlobalCheckpointHandler.TakeGlobalCheckpoint
+import edu.uci.ics.amber.engine.architecture.worker.promisehandlers.FinalizeCheckpointHandler.FinalizeCheckpoint
 import edu.uci.ics.amber.engine.architecture.worker.promisehandlers.TakeCheckpointHandler.TakeCheckpoint
-import edu.uci.ics.amber.engine.common.{
-  AmberConfig,
-  CheckpointState,
-  SerializedState,
-  VirtualIdentityUtils
-}
-import edu.uci.ics.amber.engine.common.ambermessage.{
-  ChannelID,
-  DelayedCallPayload,
-  NoAlignment,
-  WorkflowFIFOMessage
-}
+import edu.uci.ics.amber.engine.common.{AmberConfig, CheckpointState, SerializedState, VirtualIdentityUtils}
+import edu.uci.ics.amber.engine.common.ambermessage.{ChannelID, DelayedCallPayload, NoAlignment, WorkflowFIFOMessage}
 import edu.uci.ics.amber.engine.common.rpc.AsyncRPCServer.ControlCommand
 import edu.uci.ics.amber.engine.common.storage.SequentialRecordStorage
 
@@ -50,56 +41,49 @@ trait TakeGlobalCheckpointHandler {
       val storageFolder = AmberConfig.faultToleranceLogRootFolder.get
       val uri = new URI(s"$storageFolder$checkpointId/")
       val physicalOpToTakeCheckpoint = cp.workflow.physicalPlan.operators.map(_.id)
-      val collectorFutures = cp.inputGateway.getAllChannels
-        .filter { c =>
-          physicalOpToTakeCheckpoint.contains(VirtualIdentityUtils.getPhysicalOpId(c.channelId.to))
-        }
-        .map(_.collectMessagesUntilMarker(checkpointId))
+      //      val collectorFutures = cp.inputGateway.getAllChannels
+      //        .filter { c =>
+      //          physicalOpToTakeCheckpoint.contains(VirtualIdentityUtils.getPhysicalOpId(c.channelId.to))
+      //        }
+      //        .map(_.collectMessagesUntilMarker(checkpointId))
       var totalSize = 0L
-      val workerCheckpointFuture = execute(
+      execute(
         PropagateChannelMarker(
           cp.executionState.getAllOperatorExecutions.map(_._1).toSet,
           checkpointId,
           NoAlignment,
           cp.workflow.physicalPlan,
           physicalOpToTakeCheckpoint,
-          TakeCheckpoint(uri, msg.estimationOnly)
+          TakeCheckpoint(msg.estimationOnly)
         ),
         sender
-      ).map { ret =>
-        ret.foreach {
+      ).flatMap { ret =>
+        Future.collect(ret.map {
           case (workerId, chkptSize: Long) =>
             totalSize += chkptSize
+            send(FinalizeCheckpoint(checkpointId, uri), workerId)
+        }).map { _ =>
+          logger.info(s"global checkpoint finalized, size = $totalSize")
+          totalSize
         }
-      }
-      val inflightMsgCollectionFuture =
-        Future.collect(collectorFutures.toSeq).flatMap { iterables =>
-          val promise = Promise[Unit]()
-          val closure = () => {
-            if (!msg.estimationOnly) {
-              chkpt.save(SerializedState.IN_FLIGHT_MSG_KEY, iterables.flatten)
-              val storage = SequentialRecordStorage.getStorage[CheckpointState](Some(uri))
-              val writer = storage.getWriter(actorId.name)
-              writer.writeRecord(chkpt)
-              writer.flush()
-              totalSize += chkpt.size()
-            }
-            promise.setValue()
-          }
-          val channel = cp.inputGateway.getChannel(ChannelID.InternalDelayedClosureChannelID)
-          channel.acceptMessage(
-            WorkflowFIFOMessage(
-              ChannelID.InternalDelayedClosureChannelID,
-              channel.getCurrentSeq,
-              DelayedCallPayload(closure)
-            )
-          )
-          promise
-        }
-      Future.collect(Seq(workerCheckpointFuture, inflightMsgCollectionFuture)).map { _ =>
-        logger.info(s"global checkpoint finalized, size = $totalSize")
-        totalSize
       }
     }
   }
 }
+//      val inflightMsgCollectionFuture =
+//        Future.collect(collectorFutures.toSeq).flatMap { iterables =>
+//          val promise = Promise[Unit]()
+//          val closure = () => {
+//            if (!msg.estimationOnly) {
+//              chkpt.save(SerializedState.IN_FLIGHT_MSG_KEY, iterables.flatten)
+//              val storage = SequentialRecordStorage.getStorage[CheckpointState](Some(uri))
+//              val writer = storage.getWriter(actorId.name)
+//              writer.writeRecord(chkpt)
+//              writer.flush()
+//              totalSize += chkpt.size()
+//            }
+//            promise.setValue()
+//          }
+//
+//          promise
+//        }
