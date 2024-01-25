4b85aad52f2f36c86f0a2d905e6bd8cfd35c9034
Shengquan Ni <13672781+shengquan-ni@users.noreply.github.com>
2024-01-25 00:00:15 -0800

    apply change

---
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/common/storage/VFSRecordStorage.scala
@@ -0,0 +1,46 @@
+package edu.uci.ics.amber.engine.common.storage
+
+import com.typesafe.scalalogging.LazyLogging
+import edu.uci.ics.amber.engine.common.storage.SequentialRecordStorage.{
+  SequentialRecordReader,
+  SequentialRecordWriter
+}
+import org.apache.commons.vfs2.{FileObject, FileSystemManager, VFS}
+
+import java.io.{DataInputStream, DataOutputStream}
+import java.net.URI
+
+class VFSRecordStorage[T >: Null <: AnyRef](vfsLogFolderURI: URI)
+    extends SequentialRecordStorage[T]
+    with LazyLogging {
+
+  private val fs: FileSystemManager = VFS.getManager
+  private var folder: FileObject = _
+  try {
+    folder = fs.resolveFile(vfsLogFolderURI)
+  } catch {
+    case e: Throwable => throw e
+  }
+
+  if (!folder.exists()) {
+    folder.createFolder()
+  }
+
+  override def getWriter(fileName: String): SequentialRecordStorage.SequentialRecordWriter[T] = {
+    val file = folder.resolveFile(fileName)
+    file.createFile()
+    val outputStream = file.getContent.getOutputStream
+    new SequentialRecordWriter(new DataOutputStream(outputStream))
+  }
+
+  override def getReader(fileName: String): SequentialRecordStorage.SequentialRecordReader[T] = {
+    new SequentialRecordReader(() => {
+      val inputStream = folder.resolveFile(fileName).getContent.getInputStream
+      new DataInputStream(inputStream)
+    })
+  }
+
+  override def deleteStorage(): Unit = {
+    folder.deleteAll()
+  }
+}
