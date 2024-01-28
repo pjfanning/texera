aaffdb3ea8a93638bc785bfa2b5e20b5fc2bbb53
Jiadong Bai <bobbaicloudwithpants@gmail.com>
2024-01-28 10:45:31 -0800

    finish dataset viewer

--- core/new-gui/src/app/dashboard/user/component/user-environment/user-environment-datasets-viewer/user-environment-datasets-viewer.component.ts
+++ core/new-gui/src/app/dashboard/user/component/user-environment/user-environment-datasets-viewer/user-environment-datasets-viewer.component.ts
@@ -1,20 +1,98 @@
-import {Component} from "@angular/core";
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from "@angular/core";
+import {index} from "d3";
+import {EnvironmentService} from "../../../service/user-environment/environment.service";
+import {DatasetService} from "../../../service/user-dataset/dataset.service";
+import {DatasetVersionFileTreeNode} from "../../../../../common/type/datasetVersionFileTree";
+import {DashboardDataset} from "../../../type/dashboard-dataset.interface";
+import {NotificationService} from "../../../../../common/service/notification/notification.service";
 
 @Component({
     selector: 'texera-user-environment-datasets-viewer',
     templateUrl: './user-environment-datasets-viewer.component.html',
     styleUrls: ['./user-environment-datasets-viewer.component.scss']
 })
-export class UserEnvironmentDatasetsViewerComponent {
+export class UserEnvironmentDatasetsViewerComponent implements OnInit, OnChanges {
+
+    // from did to dvid
+    @Input()
+    public datasetIdToVersionId: Map<DashboardDataset, number> = new Map();
+
+    datasetFileTreeNodeLists: Map<[number, number], [string, DatasetVersionFileTreeNode[]]> = new Map();
+
     panels = [
-        { title: 'Header 1', content: 'Content 1' },
-        { title: 'Header 2', content: 'Content 2' },
-        { title: 'Header 3', content: 'Content 3' }
+        {title: 'Header 1', content: 'Content 1'},
+        {title: 'Header 2', content: 'Content 2'},
+        {title: 'Header 3', content: 'Content 3'}
     ];
 
     onMoreAction(index: number, event: MouseEvent): void {
         event.stopPropagation(); // Prevent the collapse panel from toggling
         console.log('More action clicked on panel index:', index);
         // Implement your more action logic here
+    }
+
+    private constructor(
+        private environmentService: EnvironmentService,
+        private datasetService: DatasetService,
+        private notificationService: NotificationService) {
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        if (changes.datasetIdToVersionId) {
+            // Check if the value has changed by comparing the current value to the previous value
+            const prev = changes.datasetIdToVersionId.previousValue as Map<DashboardDataset, number>;
+            const curr = changes.datasetIdToVersionId.currentValue as Map<DashboardDataset, number>;
+
+            this.handleMapChange(prev, curr);
+        }
+    }
+
+    private handleMapChange(prev: Map<DashboardDataset, number>, curr: Map<DashboardDataset, number>): void {
+        // New key-value pairs added
+        curr.forEach((currValue, currKey) => {
+            if (!prev.has(currKey)) {
+                if (currKey.dataset.did) {
+                    const did = currKey.dataset.did;
+                    this.updateNewDatasetWithVersion(did, currValue, currKey.dataset.name);
+                }
+            }
+        });
+
+        // Old keys that might have new values or be deleted
+        prev.forEach((prevValue, prevKey) => {
+            if (!curr.has(prevKey)) {
+                // Handle deleted key
+                if (prevKey.dataset.did)
+                    this.datasetFileTreeNodeLists.delete([prevKey.dataset.did, prevValue]);
+            } else if (curr.get(prevKey) !== prevValue) {
+                // Handle old key with new value
+                if (prevKey.dataset.did) {
+                    this.updateNewDatasetWithVersion(prevKey.dataset.did, prevValue, prevKey.dataset.name);
+                }
+            }
+        });
+    }
+
+
+    ngOnInit(): void {
+        this.datasetIdToVersionId.forEach((dvid, key) => {
+            if (key.dataset.did) {
+                const did = key.dataset.did;
+                this.updateNewDatasetWithVersion(did, dvid, key.dataset.name);
+            }
+        })
+    }
+
+    private updateNewDatasetWithVersion(did: number, dvid: number, datasetName: string) {
+        this.datasetService
+            .retrieveDatasetVersionFileTree(did, dvid)
+            .subscribe({
+                next: nodes => {
+                    this.datasetFileTreeNodeLists.set([did, dvid], [datasetName, nodes])
+                },
+                error: (error: unknown) => {
+                    this.notificationService.error(`Error loading dataset info`);
+                },
+            })
     }
 }
