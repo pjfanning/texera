528b043bb26bfe06c106416fa6addebfb0bb32ab
hjyy22 <hjyyyyy22@gmail.com>
2024-02-20 17:38:05 -0800

    jiayi-kernal-optimisation

--- core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/machineLearning/SVCtrainer/SVCtrainerOpDesc.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/machineLearning/SVCtrainer/SVCtrainerOpDesc.scala
@@ -1,9 +1,19 @@
 package edu.uci.ics.texera.workflow.operators.machineLearning.SVCtrainer
 
 import com.fasterxml.jackson.annotation.{JsonProperty, JsonPropertyDescription}
-import com.kjetland.jackson.jsonSchema.annotations.{JsonSchemaInject, JsonSchemaString, JsonSchemaTitle}
+import com.kjetland.jackson.jsonSchema.annotations.{
+  JsonSchemaBool,
+  JsonSchemaInject,
+  JsonSchemaString,
+  JsonSchemaTitle
+}
 import edu.uci.ics.amber.engine.common.workflow.{InputPort, OutputPort, PortIdentity}
-import edu.uci.ics.texera.workflow.common.metadata.annotations.{AutofillAttributeName, AutofillAttributeNameList, AutofillAttributeNameOnPort1, HideAnnotation}
+import edu.uci.ics.texera.workflow.common.metadata.annotations.{
+  AutofillAttributeName,
+  AutofillAttributeNameList,
+  AutofillAttributeNameOnPort1,
+  HideAnnotation
+}
 import edu.uci.ics.texera.workflow.common.metadata.{OperatorGroupConstants, OperatorInfo}
 import edu.uci.ics.texera.workflow.common.operators.PythonOperatorDescriptor
 import edu.uci.ics.texera.workflow.common.tuple.schema.{Attribute, AttributeType, Schema}
@@ -12,7 +22,7 @@ import edu.uci.ics.texera.workflow.operators.machineLearning.SVCtrainer.KernalFu
 class SVCtrainerOpDesc extends PythonOperatorDescriptor {
   @JsonProperty(defaultValue = "false")
   @JsonSchemaTitle("Using optimized C")
-  @JsonSchemaInject(json = """{"toggleHidden" : ["loop_c"]}""")
+  @JsonSchemaInject(json = """{"toggleHidden" : ["loop_c","loop_kernal"]}""")
   var is_loop: Boolean = false
 
   @JsonProperty("Selected Features")
@@ -26,7 +36,7 @@ class SVCtrainerOpDesc extends PythonOperatorDescriptor {
   @AutofillAttributeName
   var label: String = ""
 
-  @JsonProperty(required = true,defaultValue = "1")
+  @JsonProperty(required = true, defaultValue = "1")
   @JsonSchemaTitle("Custom C")
   @JsonPropertyDescription("Specify the number of nearest neighbours")
   @JsonSchemaInject(
@@ -37,12 +47,6 @@ class SVCtrainerOpDesc extends PythonOperatorDescriptor {
     )
   )
   val c: Float = Float.box(1.0f)
-
-  @JsonProperty(required = true)
-  @JsonSchemaTitle("Kernal Function")
-  @JsonPropertyDescription("multiple kernal functions")
-  var kernalFunction: KernalFunction = _
-
 
   @JsonProperty(value = "loop_c", required = false)
   @JsonSchemaTitle("Optimise c from loop")
@@ -57,40 +61,92 @@ class SVCtrainerOpDesc extends PythonOperatorDescriptor {
   @AutofillAttributeNameOnPort1
   var loop_c: String = ""
 
+  @JsonProperty(value = "loop_kernal", required = false)
+  @JsonSchemaTitle("Optimise kernal from loop")
+  @JsonPropertyDescription("Specify which attribute indicates the value of kernal")
+  @JsonSchemaInject(
+    strings = Array(
+      new JsonSchemaString(path = HideAnnotation.hideTarget, value = "is_loop"),
+      new JsonSchemaString(path = HideAnnotation.hideType, value = HideAnnotation.Type.equals),
+      new JsonSchemaString(path = HideAnnotation.hideExpectedValue, value = "false")
+    )
+  )
+  @AutofillAttributeNameOnPort1
+  var loop_kernal: String = ""
 
-    override def getOutputSchema(schemas: Array[Schema]): Schema = {
-      val outputSchemaBuilder = Schema.newBuilder
-      outputSchemaBuilder.add(new Attribute("model", AttributeType.BINARY))
-      outputSchemaBuilder.add(new Attribute("para", AttributeType.BINARY))
-      outputSchemaBuilder.add(new Attribute("features", AttributeType.BINARY)).build
+  @JsonProperty(required = true)
+  @JsonSchemaTitle("Kernal Function")
+  @JsonPropertyDescription("multiple kernal functions")
+  @JsonSchemaInject(
+    strings = Array(
+      new JsonSchemaString(path = HideAnnotation.hideTarget, value = "is_loop"),
+      new JsonSchemaString(path = HideAnnotation.hideType, value = HideAnnotation.Type.equals),
+      new JsonSchemaString(path = HideAnnotation.hideExpectedValue, value = "true")
+    )
+  )
+  var kernal: KernalFunction = KernalFunction.rbf
 
-    }
+  @JsonProperty(required = true)
+  @JsonSchemaTitle("gamma for SVC")
+  @JsonPropertyDescription("gamma for SVC")
+  @JsonSchemaInject(
+    strings = Array(
+      new JsonSchemaString(path = HideAnnotation.hideTarget, value = "kernal"),
+      new JsonSchemaString(path = HideAnnotation.hideType, value = HideAnnotation.Type.regex),
+      new JsonSchemaString(path = HideAnnotation.hideExpectedValue, value = "^linear$")
+    ),
+    bools = Array(
+      new JsonSchemaBool(path = HideAnnotation.hideOnNull, value = true)
+    )
+  )
+  var gamma: String = _
 
+  @JsonProperty(required = true)
+  @JsonSchemaTitle("coef for SVC")
+  @JsonPropertyDescription("coef for SVC")
+  @JsonSchemaInject(
+    strings = Array(
+      new JsonSchemaString(path = HideAnnotation.hideTarget, value = "kernal"),
+      new JsonSchemaString(path = HideAnnotation.hideType, value = HideAnnotation.Type.regex),
+      new JsonSchemaString(path = HideAnnotation.hideExpectedValue, value = "^linear$|^sigmod$")
+    ),
+    bools = Array(
+      new JsonSchemaBool(path = HideAnnotation.hideOnNull, value = true)
+    )
+  )
+  var coef: String = _
 
-    override def operatorInfo: OperatorInfo =
-      OperatorInfo(
-        "SVCTrainer",
-        "Train a SVM classifier",
-        OperatorGroupConstants.ML_GROUP,
-        inputPorts = List(
-          InputPort(
-            PortIdentity(0),
-            displayName = "dataset",
-            allowMultiLinks = true,
-            dependencies = List(PortIdentity(1))
-          ),
-          InputPort(PortIdentity(1), displayName = "parameter", allowMultiLinks = true),
+  override def getOutputSchema(schemas: Array[Schema]): Schema = {
+    val outputSchemaBuilder = Schema.newBuilder
+    outputSchemaBuilder.add(new Attribute("model", AttributeType.BINARY))
+    outputSchemaBuilder.add(new Attribute("para", AttributeType.BINARY))
+    outputSchemaBuilder.add(new Attribute("features", AttributeType.BINARY)).build
+  }
+
+  override def operatorInfo: OperatorInfo =
+    OperatorInfo(
+      "SVCTrainer",
+      "Train a SVM classifier",
+      OperatorGroupConstants.ML_GROUP,
+      inputPorts = List(
+        InputPort(
+          PortIdentity(0),
+          displayName = "dataset",
+          allowMultiLinks = true,
+          dependencies = List(PortIdentity(1))
         ),
-        outputPorts = List(OutputPort())
-      )
+        InputPort(PortIdentity(1), displayName = "parameter", allowMultiLinks = true)
+      ),
+      outputPorts = List(OutputPort())
+    )
 
-    override def generatePythonCode(): String = {
-      var truthy = "False"
-      if (is_loop) truthy = "True"
-      assert(selectedFeatures.nonEmpty)
-      val list_features = selectedFeatures.map(word => s""""$word"""").mkString(",")
-      val finalcode =
-        s"""
+  override def generatePythonCode(): String = {
+    var truthy = "False"
+    if (is_loop) truthy = "True"
+    assert(selectedFeatures.nonEmpty)
+    val list_features = selectedFeatures.map(word => s""""$word"""").mkString(",")
+    val finalcode =
+      s"""
            |from pytexera import *
            |
            |import pandas as pd
@@ -125,9 +181,9 @@ class SVCtrainerOpDesc extends PythonOperatorDescriptor {
            |      model_list = []
            |      para_list = []
            |      for i in range(c.shape[0]):
-           |        kernal = "$kernalFunction"
+           |        kernal = "$kernal"
            |        C= c[i]
-           |        para_str = "kernal = '$kernalFunction';C= {}".format(c[i])
+           |        para_str = "kernal = '$kernal';C= {}".format(c[i])
            |        para_list.append(para_str)
            |        model = SVC(kernel=kernal,C=C)
            |        model.fit(X_train, y_train)
@@ -143,7 +199,7 @@ class SVCtrainerOpDesc extends PythonOperatorDescriptor {
            |
            |
            |""".stripMargin
-      finalcode
-    }
+    finalcode
+  }
 
 }
