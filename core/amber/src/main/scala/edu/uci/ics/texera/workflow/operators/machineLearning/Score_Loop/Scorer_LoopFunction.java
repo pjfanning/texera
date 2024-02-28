a06604727b6a9e75573c177f1fe650f74aba114f
angelalala <angela9004@gmail.com>
2024-02-27 17:11:11 -0800

    add F1 and Recall

--- core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/machineLearning/Score_Loop/Scorer_LoopFunction.java
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/machineLearning/Score_Loop/Scorer_LoopFunction.java
@@ -5,9 +5,9 @@ import com.fasterxml.jackson.annotation.JsonValue;
 public enum Scorer_LoopFunction {
     Accuracy("Accuracy"),
     Precision_score ("Precision Score"),
-    Confusion_matrix("Confusion Matrix");
-
-
+    Confusion_matrix("Confusion Matrix"),
+    Recall_score("Recall Score"),
+    F1_score("F1 Score"),;
 
     private final String name;
     Scorer_LoopFunction(String name) {
