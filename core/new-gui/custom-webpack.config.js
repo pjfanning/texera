646ba85b7bdfb5b98e7f4387f63b6a1cff104e56
Aditya Verma <adi.kiran98@gmail.com>
2024-03-04 13:22:04 -0500

    Upgarding monaco langiage server to 7.3.0

--- core/new-gui/custom-webpack.config.js
+++ core/new-gui/custom-webpack.config.js
@@ -5,7 +5,17 @@ module.exports = {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
         include: [require("path").resolve(__dirname, "node_modules/monaco-editor")],
+        include: [require("path").resolve(__dirname, "node_modules/vscode")],
       },
+      {
+          test: /\.(mp3|wasm|ttf)$/i,
+          type: 'asset/resource'
+      }
     ],
-  },
+    parser: {
+      javascript: {
+          url: true
+      }
+    }
+  }
 };
