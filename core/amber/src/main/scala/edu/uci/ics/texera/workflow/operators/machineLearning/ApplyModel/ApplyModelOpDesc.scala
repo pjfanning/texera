95ecfbab0d4331245fe3d8860058145401394900
rr <ranr1@uci.edu>
2024-03-04 16:50:11 -0800

    support-operators-without-loop

--- core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/machineLearning/ApplyModel/ApplyModelOpDesc.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/machineLearning/ApplyModel/ApplyModelOpDesc.scala
@@ -100,11 +100,11 @@ class ApplyModelOpDesc extends PythonOperatorDescriptor {
          |        y_pred.append(y_predict)
          |        if $flag_prob:
          |            y_proba = model.predict_proba(x_test)
-         |            y_prob.append(y_proba)
+         |            y_prob.append([y_proba,model.classes_])
          |      result = dict()
          |      result['$y_pred'] = y_pred
          |      if $flag_prob:
-         |        result['$y_prob'] = (y_prob,model.classes_)
+         |        result['$y_prob'] = y_prob
          |      res  = pd.DataFrame(result)
          |      res["model"] = s["model"]
          |      res["para"] =  s["para"]
