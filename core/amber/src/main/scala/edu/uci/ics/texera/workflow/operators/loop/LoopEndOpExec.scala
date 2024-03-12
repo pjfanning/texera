2b4e2780c1b4ce4eb9c91870e7519f1cec2a60fe
hjyy22 <hjyyyyy22@gmail.com>
2024-03-12 16:16:00 -0700

    xinyuan-loop-modify

--- core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/loop/LoopEndOpExec.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/loop/LoopEndOpExec.scala
@@ -9,6 +9,7 @@ import edu.uci.ics.amber.engine.common.virtualidentity.ActorVirtualIdentity
 import edu.uci.ics.amber.engine.common.workflow.PortIdentity
 import edu.uci.ics.texera.workflow.common.operators.OperatorExecutor
 import edu.uci.ics.texera.workflow.common.tuple.Tuple
+import edu.uci.ics.texera.workflow.common.tuple.schema.Schema
 
 import scala.collection.mutable
 
@@ -30,9 +31,17 @@ class LoopEndOpExec(val workerId: ActorVirtualIdentity) extends OperatorExecutor
       case Left(t) =>
         t match {
           case t: StartOfIteration =>
+
             Iterator((EndOfIteration(t.workerId, workerId), None))
           case t =>
-            buffer.append((t, None))
+            val schema = t.asInstanceOf[Tuple].getSchema
+            if (schema.containsAttribute("Iteration")){
+              val s = new Schema.Builder(schema).removeIfExists("Iteration").build()
+              buffer.append((Tuple.newBuilder(s).addSequentially(
+                s.getAttributesScala.map(attr =>  t.asInstanceOf[Tuple].getField[Object](attr.getName)).toArray).build(), None))
+            } else {
+              buffer.append((t, None))
+            }
             Iterator()
         }
 
@@ -41,9 +50,9 @@ class LoopEndOpExec(val workerId: ActorVirtualIdentity) extends OperatorExecutor
   }
 
   override def processTexeraTuple(
-      tuple: Either[Tuple, InputExhausted],
-      input: Int,
-      pauseManager: PauseManager,
-      asyncRPCClient: AsyncRPCClient
-  ): Iterator[Tuple] = ???
+                                   tuple: Either[Tuple, InputExhausted],
+                                   input: Int,
+                                   pauseManager: PauseManager,
+                                   asyncRPCClient: AsyncRPCClient
+                                 ): Iterator[Tuple] = ???
 }
