c1f48bfdcd2cff151cb893bedac39e24da202aad
Xinyuan Lin <xinyual3@uci.edu>
2024-03-20 14:12:44 -0700

    fix fmt

--- core/amber/src/main/scala/edu/uci/ics/texera/workflow/common/operators/OperatorExecutor.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/common/operators/OperatorExecutor.scala
@@ -2,7 +2,7 @@ package edu.uci.ics.texera.workflow.common.operators
 
 import edu.uci.ics.amber.engine.common.tuple.amber.TupleLike
 import edu.uci.ics.amber.engine.common.workflow.PortIdentity
-import edu.uci.ics.texera.workflow.common.{Marker, State}
+import edu.uci.ics.texera.workflow.common.State
 import edu.uci.ics.texera.workflow.common.tuple.Tuple
 
 trait OperatorExecutor {
@@ -25,6 +25,6 @@ trait OperatorExecutor {
   }
   def onFinish(port: Int): Iterator[TupleLike] = Iterator.empty
 
-  override def onOutputFinish(port: Int): Iterator[State] = Iterator.empty
+  def onOutputFinish(port: Int): Iterator[State] = Iterator.empty
 
 }
