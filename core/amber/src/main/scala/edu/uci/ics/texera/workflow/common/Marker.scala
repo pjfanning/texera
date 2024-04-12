289787c2e478b85393232dc60e35ee58e5e62c4b
Xinyuan Lin <xinyual3@uci.edu>
2024-04-12 16:33:59 -0700

    update

--- core/amber/src/main/scala/edu/uci/ics/texera/workflow/common/Marker.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/common/Marker.scala
@@ -1,9 +1,18 @@
 package edu.uci.ics.texera.workflow.common
 
-import scala.collection.mutable
+import edu.uci.ics.amber.engine.common.tuple.amber.SpecialTupleLike
 
-sealed trait Marker {}
+sealed trait Marker extends SpecialTupleLike
 
-final case class EndOfUpstream() extends Marker
-final case class EndOfIteration() extends Marker
-final case class State(key: String, value: Any) extends Marker
+final case class EndOfUpstream() extends Marker {
+  override def getFields: Array[Any] = Array("EndOfUpstream")
+}
+
+final case class EndOfIteration() extends Marker {
+  override def getFields: Array[Any] = Array("EndOfIteration")
+}
+
+final case class State(value: String) extends Marker {
+  override def getFields: Array[Any] = Array("State")
+}
+
