2d8c1e180c8bae8a003cf6e4620dbe565f62731f
YS0meone <timyuan1016@gmail.com>
2024-07-25 17:20:41 -0700

    Merge database updates to just one script

--- core/scripts/sql/update/15.sql
+++ core/scripts/sql/update/15.sql
@@ -7,3 +7,12 @@ CREATE TABLE IF NOT EXISTS workflow_user_likes
     FOREIGN KEY (`uid`) REFERENCES `user` (`uid`) ON DELETE CASCADE,
     FOREIGN KEY (`wid`) REFERENCES `workflow` (`wid`) ON DELETE CASCADE
 ) ENGINE = INNODB;
+
+CREATE TABLE IF NOT EXISTS workflow_user_clones
+(
+    `uid` INT UNSIGNED NOT NULL,
+    `wid` INT UNSIGNED NOT NULL,
+    PRIMARY KEY (`uid`, `wid`),
+    FOREIGN KEY (`uid`) REFERENCES `user` (`uid`) ON DELETE CASCADE,
+    FOREIGN KEY (`wid`) REFERENCES `workflow` (`wid`) ON DELETE CASCADE
+) ENGINE = INNODB;
--- core/scripts/sql/update/16.sql
+++
@@ -1,9 +0,0 @@
-USE texera_db;
-CREATE TABLE IF NOT EXISTS workflow_user_clones
-(
-    `uid` INT UNSIGNED NOT NULL,
-    `wid` INT UNSIGNED NOT NULL,
-    PRIMARY KEY (`uid`, `wid`),
-    FOREIGN KEY (`uid`) REFERENCES `user` (`uid`) ON DELETE CASCADE,
-    FOREIGN KEY (`wid`) REFERENCES `workflow` (`wid`) ON DELETE CASCADE
-) ENGINE = INNODB;
