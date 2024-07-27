a8707cbb70edcae622d9918ea634ace52cad4d7f
YS0meone <timyuan1016@gmail.com>
2024-07-26 18:35:24 -0700

    Finished styling the browse section component

--- core/gui/src/app/hub/component/workflow/browse-section/hub-workflow-browse-section.component.ts
+++ core/gui/src/app/hub/component/workflow/browse-section/hub-workflow-browse-section.component.ts
@@ -1,13 +1,24 @@
 import { Component, Input } from "@angular/core";
 import { HubWorkflow } from "../../type/hub-workflow.interface";
+import { UserService } from "../../../../common/service/user/user.service";
+import { User } from "../../../../common/type/user";
+import { UntilDestroy } from "@ngneat/until-destroy";
 
-
+@UntilDestroy()
 @Component({
   selector: 'hub-workflow-browse-section',
   templateUrl: './hub-workflow-browse-section.component.html',
   styleUrls: ['./hub-workflow-browse-section.component.scss']
 })
 export class HubWorkflowBrowseSectionComponent {
-@Input() workflowList: HubWorkflow[] = [];
-@Input() sectionTitle: String = "";
+  public user: User | undefined;
+  
+  constructor(
+    private userService: UserService,
+  ){
+    this.user = this.userService.getCurrentUser();
+  }
+  @Input() workflowList: HubWorkflow[] = [];
+  @Input() sectionTitle: String = "";
+  public defaultBackground: String = "../../../../../assets/card_background.jpg";
 }
