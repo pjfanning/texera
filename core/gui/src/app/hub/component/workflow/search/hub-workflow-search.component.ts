da8837768feb550c78f626493ad39f8d988f9a22
YS0meone <timyuan1016@gmail.com>
2024-07-29 00:30:18 -0700

    Refactored the retrieve recent workflow feature

--- core/gui/src/app/hub/component/workflow/search/hub-workflow-search.component.ts
+++ core/gui/src/app/hub/component/workflow/search/hub-workflow-search.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from "@angular/core";
 import { HubWorkflowService } from "../../../service/workflow/hub-workflow.service";
 import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
 import { ActivatedRoute, Router } from "@angular/router";
-import { SearchService} from "../../../../dashboard/service/user/search.service";
-import { SearchFilterParameters } from "../../../../dashboard/type/search-filter-parameters";
 import { SortMethod } from "../../../../dashboard/type/sort-method";
 import { SearchResult } from "../../../../dashboard/type/search-result";
 import { HubWorkflow } from "../../type/hub-workflow.interface";
@@ -19,14 +17,12 @@ import { UserService } from "src/app/common/service/user/user.service";
 })
 export class HubWorkflowSearchComponent implements OnInit{
   workflowCount: number | undefined;
-  listOfWorkflows: HubWorkflow[] = [];
+  recentWorkflowList: HubWorkflow[] = [];
   popularWorkflowList: HubWorkflow[] = [];
   hasResults: boolean = false;
   public isLoggedIn: boolean = false;
   constructor(private hubWorkflowService: HubWorkflowService,
               private router: Router,
-              private searchService: SearchService,
-              private activateRouter: ActivatedRoute,
               private userService: UserService,
   ) {
     this.isLoggedIn = this.userService.isLogin();
@@ -40,52 +36,22 @@ export class HubWorkflowSearchComponent implements OnInit{
         this.workflowCount = count;
       });
     
-    this.searchWorkflows("");
-    this.hubWorkflowService.getPopularWorkflows()
+    if (this.isLoggedIn) {
+      this.hubWorkflowService
+        .getRecentWorkflows()
+        .pipe(untilDestroyed(this))
+        .subscribe(workflowList => {
+          this.recentWorkflowList = workflowList;
+        });
+    }
+
+    this.hubWorkflowService
+      .getPopularWorkflows()
       .pipe(untilDestroyed(this))
       .subscribe(workflowList => {
         this.popularWorkflowList = workflowList;
-        console.log(this.popularWorkflowList);
       });
   }
-
-  searchWorkflows(query: string): void {
-    // eslint-disable-next-line rxjs-angular/prefer-takeuntil
-    this.activateRouter.queryParams.subscribe(queryParams => {
-      const params: SearchFilterParameters = {
-        createDateStart: null,
-        createDateEnd: null,
-        modifiedDateStart: null,
-        modifiedDateEnd: null,
-        owners: [],
-        ids: [],
-        operators: [],
-        projectIds: [],
-      };
-
-      this.searchService.search([query], params, 0, 4, "workflow", SortMethod.EditTimeDesc)
-        .pipe(untilDestroyed(this))
-        .subscribe((result: SearchResult) => {
-          console.log("Search Result:", result);
-          this.listOfWorkflows = result.results
-            .filter(item => item.resourceType === "workflow" && item.workflow !== undefined)
-            .map(item => this.convertToHubWorkflow(item.workflow!));
-          this.hasResults = this.listOfWorkflows.length > 0;
-        });
-    });
-  }
-
-  private convertToHubWorkflow(dashboardWorkflow: DashboardWorkflow): HubWorkflow {
-    return {
-      name: dashboardWorkflow.workflow.name,
-      description: dashboardWorkflow.workflow.description,
-      wid: dashboardWorkflow.workflow.wid,
-      creationTime: dashboardWorkflow.workflow.creationTime,
-      lastModifiedTime: dashboardWorkflow.workflow.lastModifiedTime,
-      owner: undefined,
-    };
-  }
-
 
   navigateToSearchResult(): void {
     this.router.navigate(["/dashboard/hub/workflow/search/result"], { queryParams: { q: "" } });
