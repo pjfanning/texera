7f65e30d1373c6bdba4c5d59f22850fde767132a
YS0meone <timyuan1016@gmail.com>
2024-08-02 15:45:01 -0700

    list item style protoype

--- core/gui/src/app/dashboard/component/user/list-item/list-item.component.ts
+++ core/gui/src/app/dashboard/component/user/list-item/list-item.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+
 
 @Component({
   selector: 'texera-list-item',
   templateUrl: './list-item.component.html',
-  styleUrls: ['./list-item.component.scss']
+  styleUrls: ['./list-item.component.scss'],
 })
 export class ListItemComponent {
-
+  public entryLink: string = "";
 }
