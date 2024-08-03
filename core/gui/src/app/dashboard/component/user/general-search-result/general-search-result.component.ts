d828f051fcbf786d842bf734147000a3bd595457
YS0meone <timyuan1016@gmail.com>
2024-08-02 17:51:47 -0700

    Refactored DashboardEntry class and added a sample entry in the list component

--- core/gui/src/app/dashboard/component/user/general-search-result/general-search-result.component.ts
+++ core/gui/src/app/dashboard/component/user/general-search-result/general-search-result.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DashboardEntry } from 'src/app/dashboard/type/dashboard-entry';
+import { DashboardProject } from 'src/app/dashboard/type/dashboard-project.interface';
 
 @Component({
   selector: 'texera-general-search-result',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./general-search-result.component.scss']
 })
 export class GeneralSearchResultComponent {
+  
+  sampleProject: DashboardProject = {
+    pid: 1,
+    name: "test",
+    description: "This is a sample project.",
+    ownerID: 1,
+    creationTime: 1609459200, // Example timestamp (Unix epoch time)
+    color: "#FF5733", // Example color (Hex code)
+    accessLevel: "WRITE"
+  };
+  
+  projectEntry: DashboardEntry;
 
+  constructor() {
+    this.projectEntry = new DashboardEntry(this.sampleProject);
+  }
 }
--- core/gui/src/app/dashboard/component/user/list-item/list-item.component.ts
+++ core/gui/src/app/dashboard/component/user/list-item/list-item.component.ts
@@ -1,11 +1,49 @@
-import { Component } from "@angular/core";
+import { Component, Input, Output, OnInit } from "@angular/core";
+import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
+import { DashboardEntry } from "src/app/dashboard/type/dashboard-entry";
 
-
+@UntilDestroy()
 @Component({
   selector: 'texera-list-item',
   templateUrl: './list-item.component.html',
   styleUrls: ['./list-item.component.scss'],
 })
-export class ListItemComponent {
+export class ListItemComponent implements OnInit{
+  ROUTER_WORKFLOW_BASE_URL = "/dashboard/workspace";
+  ROUTER_USER_PROJECT_BASE_URL = "/dashboard/user-project";
+  ROUTER_DATASET_BASE_URL = "/dashboard/dataset";
   public entryLink: string = "";
+  public iconType: string = "";
+  private _entry?: DashboardEntry;
+  @Input()
+  get entry(): DashboardEntry {
+    if (!this._entry) {
+      throw new Error("entry property must be provided.");
+    }
+    return this._entry;
+  }
+  set entry(value: DashboardEntry) {
+    this._entry = value;
+  }
+  ngOnInit(): void {
+      if (this.entry.type === "workflow") {
+        this.entryLink = this.ROUTER_WORKFLOW_BASE_URL + "/" + this.entry.id;
+        this.iconType = "project";
+      }
+      else if (this.entry.type === "project") {
+        this.entryLink = this.ROUTER_USER_PROJECT_BASE_URL + '/' + this.entry.id;
+        this.iconType = "container";
+      }
+      else if (this.entry.type === "dataset") {
+        this.entryLink = this.ROUTER_DATASET_BASE_URL + '/' + this.entry.id;
+        this.iconType = "database";
+      }
+      else if (this.entry.type === "file") {
+        // not sure where to redirect
+        this.iconType = "folder-open";
+      }
+      else {
+        throw new Error("Unexpected type in DashboardEntry.");
+      }
+  }
 }
