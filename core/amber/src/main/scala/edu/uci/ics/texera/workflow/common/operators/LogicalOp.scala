79856ba5439feea24401587b815a9d9394b4cf4b
linxinyuan <linxinyuan@gmail.com>
2024-08-07 04:23:57 -0700

    update

--- core/amber/src/main/scala/edu/uci/ics/texera/workflow/common/operators/LogicalOp.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/common/operators/LogicalOp.scala
@@ -50,7 +50,7 @@ import edu.uci.ics.texera.workflow.operators.source.sql.mysql.MySQLSourceOpDesc
 import edu.uci.ics.texera.workflow.operators.source.sql.postgresql.PostgreSQLSourceOpDesc
 import edu.uci.ics.texera.workflow.operators.split.SplitOpDesc
 import edu.uci.ics.texera.workflow.operators.symmetricDifference.SymmetricDifferenceOpDesc
-import edu.uci.ics.texera.workflow.operators.state.{DualInputStateReceiverOpDesc, StateReceiverOpDesc, StateSender1OpDesc, StateSender2OpDesc, DataToStateOpDesc}
+import edu.uci.ics.texera.workflow.operators.state.{DualInputStateReceiverOpDesc, StateReceiverOpDesc, DataToStateOpDesc}
 import edu.uci.ics.texera.workflow.operators.typecasting.TypeCastingOpDesc
 import edu.uci.ics.texera.workflow.operators.udf.java.JavaUDFOpDesc
 import edu.uci.ics.texera.workflow.operators.udf.python.source.PythonUDFSourceOpDescV2
@@ -148,8 +148,6 @@ trait StateTransferFunc
     new Type(value = classOf[TypeCastingOpDesc], name = "TypeCasting"),
     new Type(value = classOf[LimitOpDesc], name = "Limit"),
     new Type(value = classOf[DataToStateOpDesc], name = "DataToState"),
-    new Type(value = classOf[StateSender1OpDesc], name = "StateSender1"),
-    new Type(value = classOf[StateSender2OpDesc], name = "State ender2"),
     new Type(value = classOf[StateReceiverOpDesc], name = "TestingStateReceiver"),
     new Type(value = classOf[DualInputStateReceiverOpDesc], name = "DualInputTestingStateReceiver"),
     new Type(value = classOf[RandomKSamplingOpDesc], name = "RandomKSampling"),
