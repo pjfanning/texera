a9b4eef31320a313c0ca22f5a47a377c043150cd
GspikeHalo <2318002579@qq.com>
2024-08-19 17:13:23 -0700

    Adds weeks as units

--- core/gui/src/app/dashboard/component/user/list-item/list-item.component.ts
+++ core/gui/src/app/dashboard/component/user/list-item/list-item.component.ts
@@ -146,15 +146,18 @@ export class ListItemComponent implements OnInit, OnChanges {
     const minutesAgo = Math.floor(timeDifference / (1000 * 60));
     const hoursAgo = Math.floor(timeDifference / (1000 * 60 * 60));
     const daysAgo = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    const weeksAgo = Math.floor(daysAgo / 7);
 
     if (minutesAgo < 60) {
       return `${minutesAgo} minutes ago`;
     } else if (hoursAgo < 24) {
       return `${hoursAgo} hours ago`;
-    } else if (daysAgo < 30) {
+    } else if (daysAgo < 7) {
       return `${daysAgo} days ago`;
+    } else if (weeksAgo < 4) {
+      return `${weeksAgo} weeks ago`;
     } else {
-      return new Date(timestamp).toLocaleDateString();  // If more than 30 days, return standard date format
+      return new Date(timestamp).toLocaleDateString();  // If more than 1 month, return standard date format
     }
   }
 }
