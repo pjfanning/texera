3f2f056bd9bcce7a6bba4685ae57e6698f283751
GspikeHalo <2318002579@qq.com>
2024-08-19 17:17:47 -0700

    format fix

--- core/gui/src/app/dashboard/component/user/list-item/list-item.component.ts
+++ core/gui/src/app/dashboard/component/user/list-item/list-item.component.ts
@@ -1,22 +1,14 @@
 import { Component, EventEmitter, Input, Output, OnInit, OnChanges, SimpleChanges } from "@angular/core";
-import { environment } from "src/environments/environment";
 import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
 import { NzModalService } from "ng-zorro-antd/modal";
-import { WorkflowExecutionHistoryComponent } from "../user-workflow/ngbd-modal-workflow-executions/workflow-execution-history.component";
 import { DashboardEntry } from "src/app/dashboard/type/dashboard-entry";
 import { ShareAccessComponent } from "../share-access/share-access.component";
-import {
-  DEFAULT_WORKFLOW_NAME,
-  WorkflowPersistService,
-} from "src/app/common/service/workflow-persist/workflow-persist.service";
+import { WorkflowPersistService } from "src/app/common/service/workflow-persist/workflow-persist.service";
 import { Workflow } from "src/app/common/type/workflow";
-import { Dataset } from "src/app/common/type/dataset";
 import { DatasetService } from "src/app/dashboard/service/user/dataset/dataset.service";
 import { FileSaverService } from "src/app/dashboard/service/user/file/file-saver.service";
-import { DashboardProject } from "src/app/dashboard/type/dashboard-project.interface";
 import { UserProjectService } from "src/app/dashboard/service/user/project/user-project.service";
 import { firstValueFrom } from "rxjs";
-import { DashboardDataset } from "src/app/dashboard/type/dashboard-dataset.interface";
 import { NotificationService } from "src/app/common/service/notification/notification.service";
 
 @UntilDestroy()
@@ -51,10 +43,7 @@ export class ListItemComponent implements OnInit, OnChanges {
   constructor(
     private modalService: NzModalService,
     private workflowPersistService: WorkflowPersistService,
-    private fileSaverService: FileSaverService,
-    private userProjectService: UserProjectService,
-    private datasetService: DatasetService,
-    private notificationService: NotificationService
+    private fileSaverService: FileSaverService
   ) {}
 
   initializeEntry() {
@@ -157,7 +146,7 @@ export class ListItemComponent implements OnInit, OnChanges {
     } else if (weeksAgo < 4) {
       return `${weeksAgo} weeks ago`;
     } else {
-      return new Date(timestamp).toLocaleDateString();  // If more than 1 month, return standard date format
+      return new Date(timestamp).toLocaleDateString();
     }
   }
 }
