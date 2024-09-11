90797e106eb592166480e2405f2c535d5e4225b0
Jiadong Bai <bobbaicloudwithpants@gmail.com>
2024-09-11 15:55:51 +0800

    add physical plan and partition info

--- core/gui/src/app/common/type/physical-plan.ts
+++ core/gui/src/app/common/type/physical-plan.ts
@@ -1,3 +1,12 @@
-export interface PhysicalOp {
+import {ExecutionIdentity, PhysicalOpIdentity, WorkflowIdentity} from "./proto/edu/uci/ics/amber/engine/common/virtualidentity";
+import {PartitionInfoUnion} from "./partition-info";
 
+export interface PhysicalOp {
+  id: PhysicalOpIdentity;
+  workflowId: WorkflowIdentity;
+  executionId: ExecutionIdentity;
+  parallelizable: boolean;
+  partitionRequirement: Array<PartitionInfoUnion | null>;
+  isOneToManyOp: boolean;
+  suggestedWorkerNum: number | null;
 }
