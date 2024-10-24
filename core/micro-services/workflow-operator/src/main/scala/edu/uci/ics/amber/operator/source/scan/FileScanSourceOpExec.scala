56851cd1b59c9787990e8c9a9d393adbfd4dcfb6
Jiadong Bai <bobbaicloudwithpants@gmail.com>
2024-10-24 09:19:31 -0700

    pass all the tests

---
+++ core/micro-services/workflow-operator/src/main/scala/edu/uci/ics/amber/operator/source/scan/FileScanSourceOpExec.scala
@@ -0,0 +1,81 @@
+package edu.uci.ics.amber.operator.source.scan
+
+import edu.uci.ics.amber.core.tuple.AttributeTypeUtils.parseField
+import edu.uci.ics.amber.core.tuple.TupleLike
+import edu.uci.ics.amber.storage.dataset.DatasetFileDocument
+import edu.uci.ics.amber.core.executor.SourceOperatorExecutor
+
+import java.io._
+import scala.collection.mutable
+import scala.jdk.CollectionConverters.IteratorHasAsScala
+import org.apache.commons.compress.archivers.{ArchiveInputStream, ArchiveStreamFactory}
+import org.apache.commons.io.IOUtils.toByteArray
+
+class FileScanSourceOpExec private[scan] (
+                                           filePath: String,
+                                           datasetFileDesc: DatasetFileDocument,
+                                           fileAttributeType: FileAttributeType,
+                                           fileEncoding: FileDecodingMethod,
+                                           extract: Boolean,
+                                           outputFileName: Boolean,
+                                           fileScanLimit: Option[Int] = None,
+                                           fileScanOffset: Option[Int] = None
+                                         ) extends SourceOperatorExecutor {
+
+  @throws[IOException]
+  override def produceTuple(): Iterator[TupleLike] = {
+    var filenameIt: Iterator[String] = Iterator.empty
+    val fileEntries: Iterator[InputStream] = {
+      val is = createInputStream(filePath, datasetFileDesc)
+      if (extract) {
+        val inputStream: ArchiveInputStream = new ArchiveStreamFactory().createArchiveInputStream(
+          new BufferedInputStream(is)
+        )
+        val (it1, it2) = Iterator
+          .continually(inputStream.getNextEntry)
+          .takeWhile(_ != null)
+          .filterNot(_.getName.startsWith("__MACOSX"))
+          .duplicate
+        filenameIt = it1.map(entry => entry.getName)
+        it2.map(_ => inputStream)
+      } else {
+        Iterator(is)
+      }
+    }
+
+    if (fileAttributeType.isSingle) {
+      fileEntries.zipAll(filenameIt, null, null).map {
+        case (entry, fileName) =>
+          val fields: mutable.ListBuffer[Any] = mutable.ListBuffer()
+          if (outputFileName) {
+            fields.addOne(fileName)
+          }
+          fields.addOne(fileAttributeType match {
+            case FileAttributeType.SINGLE_STRING =>
+              new String(toByteArray(entry), fileEncoding.getCharset)
+            case _ => parseField(toByteArray(entry), fileAttributeType.getType)
+          })
+          TupleLike(fields.toSeq: _*)
+      }
+    } else {
+      fileEntries.flatMap(entry =>
+        new BufferedReader(new InputStreamReader(entry, fileEncoding.getCharset))
+          .lines()
+          .iterator()
+          .asScala
+          .slice(
+            fileScanOffset.getOrElse(0),
+            fileScanOffset.getOrElse(0) + fileScanLimit.getOrElse(Int.MaxValue)
+          )
+          .map(line => {
+            TupleLike(fileAttributeType match {
+              case FileAttributeType.SINGLE_STRING => line
+              case _                               => parseField(line, fileAttributeType.getType)
+            })
+          })
+      )
+    }
+  }
+
+}
+
---
+++ core/micro-services/workflow-operator/src/main/scala/edu/uci/ics/amber/operator/source/scan/json/JSONLScanSourceOpExec.scala
@@ -0,0 +1,52 @@
+package edu.uci.ics.amber.operator.source.scan.json
+
+import edu.uci.ics.amber.core.tuple.AttributeTypeUtils.parseField
+import edu.uci.ics.amber.core.tuple.{Schema, TupleLike}
+import edu.uci.ics.amber.storage.dataset.DatasetFileDocument
+import edu.uci.ics.amber.util.JSONUtils.objectMapper
+import JSONUtil.JSONToMap
+import edu.uci.ics.amber.core.executor.SourceOperatorExecutor
+import edu.uci.ics.amber.operator.source.scan.FileDecodingMethod
+
+import java.io.{BufferedReader, InputStreamReader}
+import scala.jdk.CollectionConverters.IteratorHasAsScala
+import scala.util.{Failure, Success, Try}
+
+class JSONLScanSourceOpExec private[json] (
+    filePath: String,
+    datasetFileDesc: DatasetFileDocument,
+    fileEncoding: FileDecodingMethod,
+    startOffset: Int,
+    endOffset: Int,
+    flatten: Boolean,
+    schemaFunc: () => Schema
+) extends SourceOperatorExecutor {
+  private var schema: Schema = _
+  private var rows: Iterator[String] = _
+  private var reader: BufferedReader = _
+
+  override def produceTuple(): Iterator[TupleLike] = {
+    rows.flatMap { line =>
+      Try {
+        val data = JSONToMap(objectMapper.readTree(line), flatten).withDefaultValue(null)
+        val fields = schema.getAttributeNames.map { fieldName =>
+          parseField(data(fieldName), schema.getAttribute(fieldName).getType)
+        }
+        TupleLike(fields: _*)
+      } match {
+        case Success(tuple) => Some(tuple)
+        case Failure(_)     => None
+      }
+    }
+  }
+  override def open(): Unit = {
+    schema = schemaFunc()
+    reader = new BufferedReader(
+      new InputStreamReader(createInputStream(filePath, datasetFileDesc), fileEncoding.getCharset)
+    )
+    rows = reader.lines().iterator().asScala.slice(startOffset, endOffset)
+  }
+
+  override def close(): Unit = reader.close()
+
+}
