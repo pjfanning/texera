c3e2dde1897a8110cc528fd06e4c5235cc6b1403
Jiadong Bai <bobbaicloudwithpants@gmail.com>
2024-10-24 11:50:24 -0700

    cleanup the config loader

--- core/micro-services/dao/src/main/scala/edu/uci/ics/texera/DaoConfig.scala
+++ core/micro-services/dao/src/main/scala/edu/uci/ics/texera/DaoConfig.scala
@@ -8,29 +8,16 @@ import java.util.{Map => JMap}
 import scala.jdk.CollectionConverters._
 
 object DaoConfig {
-
-  private val configFile: File = new File(
-    Path
-      .of("dao")
-      .resolve("src")
-      .resolve("main")
-      .resolve("resources")
-      .resolve("dao-config.yaml")
-      .toString
-  )
-
-  // Load the configuration once during object initialization
   private val conf: Map[String, Any] = {
     val yaml = new Yaml()
     val inputStream = getClass.getClassLoader.getResourceAsStream("dao-config.yaml")
     val javaConf = yaml.load(inputStream).asInstanceOf[JMap[String, Any]].asScala.toMap
 
-    // Ensure the jdbc section is also converted to a Scala Map
+    // convert the jdbc section
     val jdbcMap = javaConf("jdbc").asInstanceOf[JMap[String, Any]].asScala.toMap
-    javaConf.updated("jdbc", jdbcMap) // Update conf with jdbc as Scala Map
+    javaConf.updated("jdbc", jdbcMap)
   }
 
-  // Extract values from the configuration file
   val jdbcUrl: String = conf("jdbc").asInstanceOf[Map[String, String]]("url")
   val jdbcUsername: String = conf("jdbc").asInstanceOf[Map[String, String]]("username")
   val jdbcPassword: String = conf("jdbc").asInstanceOf[Map[String, String]]("password")
