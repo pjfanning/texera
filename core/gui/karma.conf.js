8fa2d09e11d0d0b652a7036717bee7605b5749f8
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2024-10-25 13:25:19 -0700

    attempt

--- core/gui/karma.conf.js
+++ core/gui/karma.conf.js
@@ -1,46 +1,46 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-
-module.exports = function(config) {
+// Karma configuration file
+module.exports = function (config) {
   config.set({
     basePath: "",
     frameworks: ['waitwebpack', "jasmine", "@angular-devkit/build-angular"],
     plugins: [
       require("karma-jasmine"),
       require("karma-chrome-launcher"),
-      require("karma-coverage"),
       require("karma-jasmine-html-reporter"),
-      require('./karma.waitwebpack'),
+      require("./karma.waitwebpack"),
       require("@angular-devkit/build-angular/plugins/karma")
     ],
     client: {
-      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+      clearContext: false, // Leave Jasmine Spec Runner output visible in the browser
       jasmine: {
-        random: false // disable the random running order
-      }
+        random: false, // Disable random order for consistent test results
+      },
     },
     customLaunchers: {
       ChromeHeadlessCustom: {
-        base: 'ChromeHeadless',
+        base: "ChromeHeadless",
         flags: [
-          '--no-sandbox',
-          '--headless',
-          '--disable-gpu',
-          '--disable-translate',
-          '--disable-extensions'
+          "--no-sandbox",
+          "--headless",
+          "--disable-gpu",
+          "--disable-translate",
+          "--disable-extensions",
+          "--remote-debugging-port=9222", // Enable remote debugging for better error output
+          "--disable-dev-shm-usage", // Avoid /dev/shm issues in CI environments
         ],
       },
     },
-    reporters: ["progress", "kjhtml"],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ["ChromeHeadlessCustom"],
-    singleRun: false,
-    captureTimeout: 240000,
-    browserDisconnectTimeout: 60000,
-    browserDisconnectTolerance: 1,
-    browserNoActivityTimeout: 240000
+    reporters: ["progress", "kjhtml"], // Use basic progress and HTML reporters
+    port: 9876, // Karma's web server port
+    colors: true, // Enable colors in the output (reporters and logs)
+    logLevel: config.LOG_INFO, // Set log level
+    autoWatch: false, // Disable auto-watch to prevent re-runs in CI
+    browsers: ["ChromeHeadlessCustom"], // Run tests in headless Chrome
+    singleRun: true, // Ensure Karma exits after running tests once (useful for CI)
+    restartOnFileChange: false, // Disable file change restarts in CI
+    captureTimeout: 240000, // 4-minute timeout for capturing the browser
+    browserDisconnectTimeout: 10000, // 10-second disconnect timeout
+    browserDisconnectTolerance: 3, // Allow up to 3 disconnects before failing
+    browserNoActivityTimeout: 240000, // 4-minute no-activity timeout
   });
 };
