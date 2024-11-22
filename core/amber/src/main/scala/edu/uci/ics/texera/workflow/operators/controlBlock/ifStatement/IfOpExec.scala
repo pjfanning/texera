c44bb31fc2fdd994c57857099c78f9966da26e37
Xinyuan Lin <xinyual3@uci.edu>
2024-11-21 17:15:15 -0800

    update

--- core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/controlBlock/ifStatement/IfOpExec.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/controlBlock/ifStatement/IfOpExec.scala
@@ -6,18 +6,21 @@ import edu.uci.ics.amber.engine.common.model.tuple.{Tuple, TupleLike}
 import edu.uci.ics.amber.engine.common.workflow.PortIdentity
 
 class IfOpExec(conditionName: String) extends OperatorExecutor {
-  private var output_port: Int = _
+  private var outputPort: PortIdentity  = PortIdentity(1) // by default, it should be the true port.
 
+  //This function can handle one or more states.
+  //The state can have mutiple key-value pairs. Keys are not identified by conditionName will be ignored.
+  //It can accept any value that can be converted to a boolean. For example, Int 1 will be converted to true.
   override def processState(state: State, port: Int): Option[State] = {
-    output_port = if (state.get(conditionName).asInstanceOf[Boolean]) 1 else 0
-    None
+    outputPort = if (state.get(conditionName).asInstanceOf[Boolean]) PortIdentity(1) else PortIdentity()
+    Some(state)
   }
 
   override def processTupleMultiPort(
       tuple: Tuple,
       port: Int
   ): Iterator[(TupleLike, Option[PortIdentity])] =
-    Iterator((tuple, Some(PortIdentity(output_port))))
+    Iterator((tuple, Some(outputPort)))
 
   override def processTuple(tuple: Tuple, port: Int): Iterator[TupleLike] = ???
 }
