3bb3d04c5bc2cbcdd9a49df7c898a7efcd846079
Xinyuan Lin <xinyual3@uci.edu>
2024-11-21 21:38:09 -0800

    fix fmt

--- core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/controlBlock/ifStatement/IfOpExec.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/controlBlock/ifStatement/IfOpExec.scala
@@ -6,13 +6,14 @@ import edu.uci.ics.amber.engine.common.model.tuple.{Tuple, TupleLike}
 import edu.uci.ics.amber.engine.common.workflow.PortIdentity
 
 class IfOpExec(conditionName: String) extends OperatorExecutor {
-  private var outputPort: PortIdentity  = PortIdentity(1) // by default, it should be the true port.
+  private var outputPort: PortIdentity = PortIdentity(1) // by default, it should be the true port.
 
   //This function can handle one or more states.
   //The state can have mutiple key-value pairs. Keys are not identified by conditionName will be ignored.
   //It can accept any value that can be converted to a boolean. For example, Int 1 will be converted to true.
   override def processState(state: State, port: Int): Option[State] = {
-    outputPort = if (state.get(conditionName).asInstanceOf[Boolean]) PortIdentity(1) else PortIdentity()
+    outputPort =
+      if (state.get(conditionName).asInstanceOf[Boolean]) PortIdentity(1) else PortIdentity()
     Some(state)
   }
 
