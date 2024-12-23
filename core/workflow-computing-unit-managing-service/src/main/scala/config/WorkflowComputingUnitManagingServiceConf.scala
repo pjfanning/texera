b02e4599e5a328b1d9bf1484e99d686b777d0a5e
Jiadong Bai <bobbaicloudwithpants@gmail.com>
2024-12-23 14:03:32 -0800

    change yaml of manager to conf

--- core/workflow-computing-unit-managing-service/src/main/scala/config/WorkflowComputingUnitManagingServiceConf.scala
+++ core/workflow-computing-unit-managing-service/src/main/scala/config/WorkflowComputingUnitManagingServiceConf.scala
@@ -1,26 +1,13 @@
 package config
 
-import org.yaml.snakeyaml.Yaml
-
-import java.util.{Map => JMap}
-import scala.jdk.CollectionConverters._
+import com.typesafe.config.{Config, ConfigFactory}
 
 object WorkflowComputingUnitManagingServiceConf {
 
-  private val conf: Map[String, Any] = {
-    val yaml = new Yaml()
-    val inputStream =
-      getClass.getClassLoader.getResourceAsStream("computing-unit-manager-config.yaml")
-    val javaConf = yaml.load(inputStream).asInstanceOf[JMap[String, Any]].asScala.toMap
+  // Load the configuration
+  private val conf: Config = ConfigFactory.load()
 
-    val kubernetesMap = javaConf("kubernetes").asInstanceOf[JMap[String, Any]].asScala.toMap
-    javaConf.updated("kubernetes", kubernetesMap)
-  }
-
-  val computeUnitPoolNamespace: String = conf("kubernetes")
-    .asInstanceOf[Map[String, Any]]("compute-unit-pool-namespace")
-    .asInstanceOf[String]
-  val computeUnitImageName: String = conf("kubernetes")
-    .asInstanceOf[Map[String, Any]]("imageName")
-    .asInstanceOf[String]
+  // Access the Kubernetes settings with environment variable fallback
+  val computeUnitPoolNamespace: String = conf.getString("kubernetes.compute-unit-pool-namespace")
+  val computeUnitImageName: String = conf.getString("kubernetes.image-name")
 }
